e79eacc203ecab24bcbc969483047b0b06e206f3
Paulina Koch <paulina.koch@MB8839.local>
2014-04-09 17:49:12 +0100

    Basic models.

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from game.models import Class, Student, Guardian, Teacher, School
 
-# Register your models here.
+admin.site.register(Class)
+admin.site.register(Student)
+admin.site.register(Guardian)
+admin.site.register(Teacher)
+admin.site.register(School)
+
