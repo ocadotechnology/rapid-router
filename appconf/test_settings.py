21017e1bf8a90c406eb3b68c38588697a7ea5141
Ben Cardy <ben.cardy@gmail.com>
2014-04-09 16:47:03 +0100

    Added dependency for compressor

---
+++ appconf/test_settings.py
@@ -0,0 +1,19 @@
+SITE_ID = 1
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'appconf.tests',
+]
+
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+SECRET_KEY = 'local'
