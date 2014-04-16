f46efb5bb98ab0193fa54e986d66509d7c29b064
Paulina Koch <kopalinka@wp.pl>
2014-04-16 16:17:46 +0100

    Changed the logged_students to show all the logged in students (based on the method in the model. Added students_in_class to browse through all the students in the class.

--- game/urls.py
+++ game/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'game.views.game'),
     url(r'^logged_students$', 'game.views.logged_students'),
+	url(r'^students_in_class$', 'game.views.students_in_class'),
     url(r'^submit_commands$', 'game.views.submit_commands'),
     url(r'^submit_reply$', 'game.views.submit_reply',),
 )
--- game/views.py
+++ game/views.py
@@ -10,12 +10,21 @@ def game(request):
     return render(request, 'game/game.html')
 
 def logged_students(request):
+	klass = request.user.teacher.class_teacher.all()[0]
+	students = klass.get_logged_in_students()
+	context = context = RequestContext(request, {
+		'students' : students
+	})
+	return render(request, 'game/logged_students.html', context)
+
+
+def students_in_class(request):
 	user = request.user
 	message = "Choose a class you want to see."
 	students = []
 	if request.method == 'POST':
 		selected_item = get_object_or_404(Class, id=request.POST.getlist('classes')[0])
-		students = selected_item.get_logged_in_students()
+		students = selected_item.students.all()
 	try:
 		classes = user.teacher.class_teacher.all()
  	except ObjectDoesNotExist:
@@ -27,7 +36,7 @@ def logged_students(request):
 		'students' : students
 	})
 
-	return render(request, 'game/logged_students.html', context)
+	return render(request, 'game/students_in_class.html', context)
 
 def submit_commands(request):
 	if request.method == 'POST':
