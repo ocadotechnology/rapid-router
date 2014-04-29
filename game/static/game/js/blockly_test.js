1cddfb0fb61629a5bec28ea150c762a95c58fa2f
Anders Schuller <andersschuller@gmail.com>
2014-04-29 20:09:45 +0100

    Cleaned up blockly_test.js a bit

--- game/static/game/js/blockly_test.js
+++ game/static/game/js/blockly_test.js
@@ -15,32 +15,27 @@ Blockly.Blocks['start'] = {
 };
 
 Blockly.Blocks['move_van'] = {
-  // Block for moving forward
-  init: function() {
-    this.setColour(160);
-    this.appendDummyInput()
-        .appendField('\u2191');
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setTooltip('Move the van forwards');
-  }
-};
-
-Blockly.JavaScript['move_van'] = function(block) {
-    // Generate JavaScript for moving forward
-    return 'BlocklyTest.moveForward();\n';
+    // Block for moving forward
+    init: function() {
+        this.setColour(160);
+        this.appendDummyInput()
+            .appendField('\u2191');
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip('Move the van forwards');
+    }
 };
 
 Blockly.Blocks['turn_left'] = {
-  // Block for turning left or right.
-  init: function() {
-    this.setColour(160);
-    this.appendDummyInput()
-        .appendField('\u21BA');
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setTooltip('Turn the van left');
-  }
+    // Block for turning left or right.
+    init: function() {
+        this.setColour(160);
+        this.appendDummyInput()
+            .appendField('\u21BA');
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip('Turn the van left');
+    }
 };
 
 Blockly.Blocks['turn_right'] = {
@@ -55,22 +50,17 @@ Blockly.Blocks['turn_right'] = {
     }
 };
 
-Blockly.JavaScript['turn_van'] = function(block) {
-    // Generate JavaScript for turning left or right.
-    return 'BlocklyTest.' + block.getFieldValue('DIR') + '()\n';
-};
-
 Blockly.Blocks['road_exists'] = {
-  init: function() {
-    var BOOLEANS =
-        [['road exists forward', 'FORWARD'],
-         ['road exists left', 'LEFT'],
-         ['road exists right', 'RIGHT']];
-    this.setColour(210);
-    this.setOutput(true, 'Boolean');
-    this.appendDummyInput()
-    	.appendField(new Blockly.FieldDropdown(BOOLEANS), 'CHOICE');
-  }
+    init: function() {
+        var BOOLEANS =
+            [['road exists forward', 'FORWARD'],
+             ['road exists left', 'LEFT'],
+             ['road exists right', 'RIGHT']];
+        this.setColour(210);
+        this.setOutput(true, 'Boolean');
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldDropdown(BOOLEANS), 'CHOICE');
+    }
 };
 
 var BlocklyTest = {};
@@ -94,56 +84,22 @@ BlocklyTest.addBlockToEndOfProgram = function(typeOfBlockToAdd) {
 };
 
 BlocklyTest.init = function() {
-    Blockly.inject(document.getElementById('blockly'),
-        {path: '/static/game/js/blockly/', toolbox: document.getElementById('toolbox')});
+    var blockly = document.getElementById('blockly');
+    var toolbox = document.getElementById('toolbox');
+    Blockly.inject(blockly, {
+        path: '/static/game/js/blockly/',
+        toolbox: toolbox
+    });
 
     BlocklyTest.createBlock('start');
-} ;
+};
 
 window.addEventListener('load', BlocklyTest.init);
-
-BlocklyTest.queue = [];
-
-BlocklyTest.animate = function() {
-    var instruction = BlocklyTest.queue.shift();
-    if (!instruction) {
-        return;
-    }
-    switch (instruction) {
-        case FORWARD:
-            moveForward(BlocklyTest.animate);
-            break;
-
-        case TURN_LEFT:
-            moveLeft(BlocklyTest.animate);
-            break;
-
-        case TURN_RIGHT:
-            moveRight(BlocklyTest.animate);
-            break;
-    } 
-};
-
-BlocklyTest.addInstruction = function(instruction) {
-    BlocklyTest.queue.push(instruction);
-};
-
-BlocklyTest.moveForward = function() {
-    BlocklyTest.addInstruction(FORWARD);
-};
-
-BlocklyTest.moveLeft = function() {
-    BlocklyTest.addInstruction(TURN_LEFT);
-};
-
-BlocklyTest.moveRight = function() {
-    BlocklyTest.addInstruction(TURN_RIGHT);
-};
 
 BlocklyTest.getStartBlock = function() {
     var startBlock = null;
     Blockly.mainWorkspace.getTopBlocks().forEach(function (block) {
-        if (block.type == 'start') {
+        if (block.type === 'start') {
             startBlock = block;
         }
     });
@@ -155,31 +111,31 @@ BlocklyTest.populateProgram = function() {
     	var commands = [];
     	
     	while(block){
-    		if (block.type == 'move_van') {
+    		if (block.type === 'move_van') {
     			commands.push(FORWARD_COMMAND);
-            } else if (block.type == 'turn_left') {
+            } else if (block.type === 'turn_left') {
             	commands.push(TURN_LEFT_COMMAND);
-            } else if (block.type == 'turn_right') {
+            } else if (block.type === 'turn_right') {
             	commands.push(TURN_RIGHT_COMMAND);
-            } else if (block.type == 'controls_repeat') {
+            } else if (block.type === 'controls_repeat') {
             	commands.push(
             			new While(
             					counterCondition(block.inputList[0].fieldRow[1].text_), 
             					getCommandsAtThisLevel(block.inputList[1].connection.targetBlock())));
-            } else if (block.type == 'controls_if') {
+            } else if (block.type === 'controls_if') {
             	var conditionalCommandSets = [];
             	
             	var i = 0;
             	while(i < block.inputList.length - block.elseCount_){
             		var input = block.inputList[i];
             		
-            		if(input.name.indexOf('IF') == 0) {
+            		if(input.name.indexOf('IF') === 0) {
             			var conditionBlock = input.connection.targetBlock();
             			if(conditionBlock.type = 'road_exists'){
             				var selection = conditionBlock.inputList[0].fieldRow[1].value_;
             				var condition = roadCondition(selection);
             			}
-            		} else if(input.name.indexOf('DO') == 0){
+            		} else if(input.name.indexOf('DO') === 0){
             			var conditionalCommandSet = {};
             			conditionalCommandSet.condition = condition;
             			conditionalCommandSet.commands = getCommandsAtThisLevel(input.connection.targetBlock());
@@ -189,7 +145,7 @@ BlocklyTest.populateProgram = function() {
             		i++;
             	}
             	
-            	if(block.elseCount_ == 1){
+            	if(block.elseCount_ === 1){
             		var elseCommands = getCommandsAtThisLevel(block.inputList[block.inputList.length - 1].connection.targetBlock());
             	}
             	
