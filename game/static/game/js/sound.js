12b20a253a9f61b34d3b6e7faee6580e86b06f3e
Paul Heideman <paul.heideman@ocado.com>
2014-05-07 17:54:05 +0100

    added stubs for playing sounds for the van

---
+++ game/static/game/js/sound.js
@@ -0,0 +1,15 @@
+var ocargo = ocargo || {};
+
+ocargo.sound = {}
+
+ocargo.sound.starting = function() {
+    console.debug("Playing 'starting' sound.");
+};
+
+ocargo.sound.moving = function() {
+    console.debug("Playing 'moving' sound.");
+};
+
+ocargo.sound.turning = function() {
+    console.debug("Playing 'turning' sound.");
+};
