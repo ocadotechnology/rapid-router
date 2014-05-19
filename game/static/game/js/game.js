adb539bdadabbe2ad9be215ff77191628fac45e4
Paulina Koch <kopalinka@wp.pl>
2014-05-19 14:55:03 +0100

    Changed game.js to use space indentation.

--- game/static/game/js/game.js
+++ game/static/game/js/game.js
@@ -5,26 +5,26 @@ function createUi() {
 }
 
 function createDefaultLevel(path, ui) {
-	var nodes = generateNodes(path);
-	var map = new ocargo.Map(nodes, nodes[nodes.length - 1], ui);
-	var van = new ocargo.Van(nodes[0], nodes[1], ui);
-	return new ocargo.Level(map, van, ui);
+    var nodes = generateNodes(path);
+    var map = new ocargo.Map(nodes, nodes[nodes.length - 1], ui);
+    var van = new ocargo.Van(nodes[0], nodes[1], ui);
+    return new ocargo.Level(map, van, ui);
 }
 
 function generateNodes(points){
-	var previousNode = null;
-	var nodes = [];
-	for (var i = 0; i < points.length; i++) {
-	      var p = points[i];
-	      var coordinate = new ocargo.Coordinate(p[0], p[1]);
-	      var node = new ocargo.Node(coordinate);
-	      if (previousNode) {
-	          node.addConnectedNodeWithBacklink(previousNode);
-	      }
-	      previousNode = node;
-	      nodes.push(node);
-	}
-	return nodes;
+    var previousNode = null;
+    var nodes = [];
+    for (var i = 0; i < points.length; i++) {
+          var p = points[i];
+          var coordinate = new ocargo.Coordinate(p[0], p[1]);
+          var node = new ocargo.Node(coordinate);
+          if (previousNode) {
+              node.addConnectedNodeWithBacklink(previousNode);
+          }
+          previousNode = node;
+          nodes.push(node);
+    }
+    return nodes;
 }
 
 function loadDefaultProgram() {
@@ -47,10 +47,10 @@ function loadDefaultProgram() {
 }
 
 function initialiseDefault() {
-	'use strict';
+    'use strict';
     var path = JSON.parse(PATH);
-	ocargo.ui = createUi();
-	ocargo.level = createDefaultLevel(path, ocargo.ui);
+    ocargo.ui = createUi();
+    ocargo.level = createDefaultLevel(path, ocargo.ui);
     ocargo.level.levelId = JSON.parse(LEVEL_ID);
     if ($.cookie("muted") == "true") {
         $('#mute').text("Unmute");
@@ -59,20 +59,20 @@ function initialiseDefault() {
 }
 
 function trackDevelopment() {
-	$('#moveForward').click(function() {
-		ocargo.blocklyControl.addBlockToEndOfProgram('move_van');
+    $('#moveForward').click(function() {
+        ocargo.blocklyControl.addBlockToEndOfProgram('move_van');
     });
-	
+    
     $('#turnLeft').click(function() {
-		ocargo.blocklyControl.addBlockToEndOfProgram('turn_left');
+        ocargo.blocklyControl.addBlockToEndOfProgram('turn_left');
     });
 
     $('#turnRight').click(function() {
-		ocargo.blocklyControl.addBlockToEndOfProgram('turn_right');
-	});
+        ocargo.blocklyControl.addBlockToEndOfProgram('turn_right');
+    });
     
     $('#quit').click(function(){
-    	window.location.replace('/game/');
+        window.location.replace('/game/');
     });
     
     $('#play').click(function() {
@@ -81,15 +81,15 @@ function trackDevelopment() {
         var nodes = ocargo.level.map.nodes;
         ocargo.level.van = new ocargo.Van(nodes[0], nodes[1], ocargo.ui);
         ocargo.ui.setVanToFront();
-    	ocargo.level.play(program);
+        ocargo.level.play(program);
         ocargo.level.correct = 0;
-	});
+    });
 
     $('#loadDefaultProgram').click(function() {
         loadDefaultProgram();
     });
 
-	$('#clearIncorrect').click(function() {
+    $('#clearIncorrect').click(function() {
         ocargo.blocklyControl.removeWrong();
     });
 
@@ -103,20 +103,20 @@ function trackDevelopment() {
     });
     
     $('#slideBlockly').click(function() {
-    	var c = $('#programmingConsole');
-    	if(c.is(':visible')){
-    		$('#paper').animate({width: '100%'});
-    		$('#sliderControls').animate({left: '0%'});
-    	}else{
-    		$('#paper').animate({width: '50%'});
-    		$('#sliderControls').animate({left: '50%'});
-    	}
-    	c.animate({width: 'toggle'});
+        var c = $('#programmingConsole');
+        if(c.is(':visible')){
+            $('#paper').animate({width: '100%'});
+            $('#sliderControls').animate({left: '0%'});
+        }else{
+            $('#paper').animate({width: '50%'});
+            $('#sliderControls').animate({left: '50%'});
+        }
+        c.animate({width: 'toggle'});
     });
 }
 
 $(function() {
-   	initialiseDefault();
+    initialiseDefault();
     trackDevelopment();
 });
 
@@ -132,9 +132,9 @@ $('#mute').click(function() {
 });
 
 $('#randomRoad').click(function() {
-	var points = generateRandomPathPoints([0,3], 0.5, 13);
-	var nodes = generateNodes(points);  
-	var van = new ocargo.Van(nodes[0], nodes[1], ocargo.ui);
-	var map = new ocargo.Map(nodes, nodes[nodes.length - 1], ocargo.ui);
-	ocargo.level = new ocargo.Level(map, van, ocargo.ui);
+    var points = generateRandomPathPoints([0,3], 0.5, 13);
+    var nodes = generateNodes(points);  
+    var van = new ocargo.Van(nodes[0], nodes[1], ocargo.ui);
+    var map = new ocargo.Map(nodes, nodes[nodes.length - 1], ocargo.ui);
+    ocargo.level = new ocargo.Level(map, van, ocargo.ui);
 });
