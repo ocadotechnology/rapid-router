4d0a27790c54760b05d27e754a2fb5a752265bb5
Chris Brett <chrisbrett665@gmail.com>
2014-05-21 19:03:57 +0100

    added login functionality

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.contrib.auth.views import login
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^game/', include('game.urls')),
     url(r'^reports/', include('reports.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', login),
 )
