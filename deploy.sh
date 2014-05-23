aec13972ddd32e1500089b7cb1bfc7828d1985ec
Paul Heideman <paul.heideman@ocado.com>
2014-05-23 12:35:56 +0000

    Made some changes for script to work with jenkins

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # To be used to docker deployment environment
-cd /opt/ocargo
 export DEPLOYMENT=1
-./manage.py flush --noinput
-./manage.py syncdb --noinput
 ./manage.py collectstatic --noinput
 ./manage.py compress -f
-appcfg.py update --oauth2 --noauth_local_webserver .
+./manage.py flush --noinput
+./manage.py syncdb --noinput
+appcfg.py update --authenticate_service_account .
