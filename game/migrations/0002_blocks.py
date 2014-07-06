9822ee72380eec819e8e8f1c49e9c21dee9a3b92
Paul Heideman <paul.heideman@ocado.com>
2014-07-06 21:37:50 +0100

    fixed order of blocks

--- game/migrations/0002_blocks.py
+++ game/migrations/0002_blocks.py
@@ -5,8 +5,17 @@ from django.db import models, migrations
 
 def initial_blocks(apps, schema_editor):
     Block = apps.get_model('game', 'Block')
-    types = ["turn_left", "turn_right", "turn_around", "controls_repeat", "controls_whileUntil",
-            "controls_if", "logic_negate", "road_exists", "dead_end", "at_destination", "move_forwards"]
+    types = ["move_forwards",
+            "turn_left",
+            "turn_right",
+            "turn_around",
+            "controls_repeat",
+            "controls_whileUntil",
+            "controls_if",
+            "logic_negate",
+            "road_exists",
+            "dead_end",
+            "at_destination"]
     for t in types:
         b = Block(type=t)
         b.save()
