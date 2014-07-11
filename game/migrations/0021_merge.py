c06f9631c69428a4db9b62a0021bfa288144479f
Matthew Daggitt <matthew.daggitt@ocado.com>
2014-07-11 10:46:39 +0100

    Merged migrations

---
+++ game/migrations/0021_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('game', '0020_remove_episode_r_blocks'),
+        ('game', '0020_merge'),
+    ]
+
+    operations = [
+    ]
