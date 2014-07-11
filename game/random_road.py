72a1013c607e817d263bb77228c2bdb905a2c76e
Matthew Daggitt <matthew.daggitt@ocado.com>
2014-07-11 11:58:09 +0100

    Added a 'curviness' parameter to the random map generation. Also reduced 'join_up_loops' method to O(n) as per code comment.

--- game/random_road.py
+++ game/random_road.py
@@ -6,18 +6,19 @@ from models import Episode, Level, Block
 import random
 
 Node = namedtuple('Node', ['x', 'y'])
-
+DIRECTIONS = {(1, 0), (0, 1), (-1, 0), (0, -1)}
+WIDTH = 10
+HEIGHT = 8
 
 def create(episode=None):
 
     if episode is None:
-        path = generate_random_path(Node(0, 3), 20, 0.2, 0.1)
+        path = generate_random_path(Node(0, 3), 20, 0.2, 0.1, 0.5)
         destination = json.dumps(path[-1]['coordinate'])
         level = Level(name=3000, path=json.dumps(path), max_fuel=30, destination=destination)
         
     else:
-        print(episode.__dict__)
-        path = generate_random_path(Node(0,3), episode.r_num_tiles,episode.r_branchiness,episode.r_loopiness)
+        path = generate_random_path(Node(0,3), episode.r_num_tiles, episode.r_branchiness, episode.r_loopiness, 0.0)
         destination = json.dumps(path[-1]['coordinate'])
         level = Level(name="Random level for " + episode.name + ".", path=json.dumps(path), max_fuel=30, destination=destination)
 
@@ -27,17 +28,12 @@ def create(episode=None):
 
     return level
 
-
-def calculate_node_angle(node_1, node_2):
-    return math.atan2(node_2.y - node_1.y, node_2.x - node_1.x)
-
-
-def generate_random_path(start_position, num_road_tiles, branchiness_factor, loopiness_factor):
+def generate_random_path(start_position, num_road_tiles, branchiness_factor, loopiness_factor, curviness_factor):
     nodes = [start_position]
     connections = defaultdict(list)
 
     for _ in xrange(num_road_tiles):
-        (previous_node, new_node) = pick_adjacent_node(nodes, branchiness_factor)
+        (previous_node, new_node) = pick_adjacent_node(nodes, connections, branchiness_factor, curviness_factor)
         if not new_node:
             continue
 
@@ -56,24 +52,23 @@ def generate_random_path(start_position, num_road_tiles, branchiness_factor, loo
 
     return result
 
-
-def pick_adjacent_node(nodes, branchiness_factor):
+def pick_adjacent_node(nodes, connections, branchiness_factor, curviness_factor):
+    
     for attempts in xrange(5):
         origin = pick_origin_node(nodes, branchiness_factor)
         possibles = []
 
         x = origin.x
         y = origin.y
-        for (delta_x, delta_y) in {(-1, 0), (1, 0), (0, -1), (1, 0)}:  # TODO: Make this set a constant
+        for (delta_x, delta_y) in DIRECTIONS:
             node = Node(x + delta_x, y + delta_y)
             if is_possible(node, nodes):
                 possibles.append(node)
 
         if possibles:
-            return origin, random.choice(possibles)
+            return origin, pick_destination_node(nodes, connections, origin, possibles, curviness_factor)
 
     return None, None
-
 
 def pick_origin_node(nodes, branchiness_factor):
     if random.random() < branchiness_factor:
@@ -81,28 +76,34 @@ def pick_origin_node(nodes, branchiness_factor):
     else:
         return nodes[-1]
 
+def pick_destination_node(nodes, connections, origin, possibles, curviness_factor):
+    existing_connections = [nodes[nodeIndex] for nodeIndex in connections[nodes.index(origin)]]
+    existing_connection_directions = [(node.x-origin.x,node.y-origin.y) for node in existing_connections]
+    linear = [node for node in possibles if (origin.x-node.x,origin.y-node.y) in existing_connection_directions]
+    curved = [node for node in possibles if (origin.x-node.x,origin.y-node.y) not in existing_connection_directions]
+
+    if linear and curved:
+        if random.random() < curviness_factor:
+            pick_from = curved
+        else:
+            pick_from = linear
+    else:
+        pick_from = possibles
+
+    return random.choice(pick_from)
+
 
 def join_up_loops(nodes, connections, loopiness_factor):
-    for node_index, node in enumerate(nodes):
-        for adjacent_node_index, adjacent_node in enumerate(nodes):  # TODO: This can _surely_ be improved!
-            if adjacent_node == node or not are_adjacent(node, adjacent_node) or adjacent_node_index in connections[node_index]:
-                continue
+    nodesByLocation = {(node.x,node.y):(index,node) for index, node in enumerate(nodes)}
 
-            if random.random() < loopiness_factor:
-                connections = add_new_connections(nodes, connections, node, adjacent_node)
+    for node_index, node in enumerate(nodes):
+        for location in get_neighbouring_locations(node):
+            if location in nodesByLocation:
+                adjacent_node_index, adjacent_node = nodesByLocation[location]
+                if adjacent_node_index not in connections[node_index] and random.random() < loopiness_factor:
+                    connections = add_new_connections(nodes, connections, node, adjacent_node)
 
     return connections
-
-
-def are_adjacent(node_1, node_2):
-    delta_x = node_2.x - node_1.x
-    delta_y = node_2.y - node_1.y
-    return (delta_x, delta_y) in {(-1, 0), (1, 0), (0, -1), (1, 0)}
-
-
-def is_possible(node, nodes):
-    return (node not in nodes) and 0 < node.x < 10 - 1 and 0 < node.y < 8 - 1
-
 
 def add_new_connections(nodes, connections, node_1, node_2):
     node_1_index = nodes.index(node_1)
@@ -111,3 +112,25 @@ def add_new_connections(nodes, connections, node_1, node_2):
     connections[node_2_index].append(node_1_index)
 
     return connections
+
+
+def are_adjacent(node_1, node_2):
+    delta_x = node_2.x - node_1.x
+    delta_y = node_2.y - node_1.y
+    return (delta_x, delta_y) in DIRECTIONS
+
+def calculate_node_angle(node_1, node_2):
+    return math.atan2(node_2.y - node_1.y, node_2.x - node_1.x)
+
+def is_possible(node, nodes):
+    return (node not in nodes) and 0 < node.x < WIDTH - 1 and 0 < node.y < HEIGHT - 1
+
+def get_neighbouring_locations(node):
+    squares = []
+    for delta_x, delta_y in DIRECTIONS:
+        new_x = node.x + delta_x
+        new_y = node.y + delta_y
+
+        if new_x >= 0 and new_x < WIDTH and new_y >= 0 and new_y < HEIGHT:
+            squares.append((new_x,new_y))
+    return squares
