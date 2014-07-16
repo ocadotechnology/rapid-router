596ce983d7acfa27af4ddc42a5d1a9759ebda948
Chris Brett <chrisbrett665@gmail.com>
2014-07-16 19:14:36 +0100

    added blockly to python conversion

--- game/static/game/js/blockly_control.js
+++ game/static/game/js/blockly_control.js
@@ -25,6 +25,11 @@ Blockly.Blocks['start'] = {
     }
 };
 
+Blockly.Python['start'] = function(block) {
+	return 'import van\n\
+v = van.Van()\n';
+};
+
 Blockly.Blocks['move_forwards'] = {
     // Block for moving forward
     init: function() {
@@ -38,6 +43,10 @@ Blockly.Blocks['move_forwards'] = {
         this.setNextStatement(true);
         this.setTooltip('Move the van forwards');
     }
+};
+
+Blockly.Python['move_forwards'] = function(block) {
+	return 'v.move_forward()\n';
 };
 
 Blockly.Blocks['turn_left'] = {
@@ -58,6 +67,10 @@ Blockly.Blocks['turn_left'] = {
     }
 };
 
+Blockly.Python['turn_left'] = function(block) {
+	return 'v.turn_left()\n';
+};
+
 Blockly.Blocks['turn_right'] = {
     // Block for turning right
     init: function() {
@@ -74,6 +87,10 @@ Blockly.Blocks['turn_right'] = {
         this.setNextStatement(true);
         this.setTooltip('Turn the van right');
     }
+};
+
+Blockly.Python['turn_right'] = function(block) {
+	return 'v.turn_right()\n';
 };
 
 Blockly.Blocks['turn_around'] = {
@@ -94,6 +111,10 @@ Blockly.Blocks['turn_around'] = {
     }
 };
 
+Blockly.Python['turn_around'] = function(block) {
+	return 'v.turn_around()\n';
+};
+
 Blockly.Blocks['wait'] = {
     // Block for not moving the van for a time
     init: function() {
@@ -112,6 +133,10 @@ Blockly.Blocks['wait'] = {
     }
 };
 
+Blockly.Python['wait'] = function(block) {
+	return 'v.wait()\n';
+};
+
 Blockly.Blocks['road_exists'] = {
     init: function() {
         var BOOLEANS =
@@ -126,6 +151,18 @@ Blockly.Blocks['road_exists'] = {
                                                 ocargo.blocklyControl.EXTRA_BLOCK_WIDTH,
                                                 ocargo.blocklyControl.BLOCK_HEIGHT));
     }
+};
+
+Blockly.Python['road_exists'] = function(block) {
+	if(block.inputList[0].fieldRow[1].value_ === 'FORWARD'){
+		var python = 'v.is_road_forward()';
+	}else if(block.inputList[0].fieldRow[1].value_ === 'LEFT'){
+		var python = 'v.is_road_left()';
+	}else{
+		var python = 'v.is_road_right()';
+	}
+	
+	return [python, Blockly.Python.ORDER_NONE]; //TODO: figure out what this ordering relates to
 };
 
 Blockly.Blocks['traffic_light'] = {
@@ -143,6 +180,18 @@ Blockly.Blocks['traffic_light'] = {
     }
 };
 
+Blockly.Python['traffic_light'] = function(block) {
+	if(block.inputList[0].fieldRow[1].value_ === ocargo.TrafficLight.RED){
+		var python = 'v.at_red_traffic_light()';
+	}else{
+		var python = 'v.at_green_traffic_light()';
+	}
+	
+	return [python, Blockly.Python.ORDER_NONE]; //TODO: figure out what this ordering relates to
+};
+
+//TODO: Python generation
+
 Blockly.Blocks['dead_end'] = {
     init: function() {
         this.setColour(210);
@@ -155,6 +204,10 @@ Blockly.Blocks['dead_end'] = {
     }
 };
 
+Blockly.Python['dead_end'] = function(block) {
+	return ['v.at_dead_end()', Blockly.Python.ORDER_NONE]; //TODO: figure out what this ordering relates to
+};
+
 Blockly.Blocks['at_destination'] = {
     init: function() {
         this.setColour(210);
@@ -165,6 +218,10 @@ Blockly.Blocks['at_destination'] = {
                                                 ocargo.blocklyControl.EXTRA_BLOCK_WIDTH,
                                                 ocargo.blocklyControl.BLOCK_HEIGHT));
     }
+};
+
+Blockly.Python['at_destination'] = function(block) {
+	return ['v.at_destination()', Blockly.Python.ORDER_NONE]; //TODO: figure out what this ordering relates to;
 };
 
 Blockly.Blocks['call_proc'] = {
@@ -216,6 +273,11 @@ controlsRepeatBlock.init = function () {
         return n;
     };
 };
+
+//FIXME: dodgy hack on account of addLoopTrap not existing...
+//I think the version of python_compressed.js we are using may be incompatible
+//with the version of the rest of blockly.
+Blockly.Python.addLoopTrap = function(branch){return branch};
 
 // Make 'not' taller
 var notBlock = Blockly.Blocks['logic_negate'];
