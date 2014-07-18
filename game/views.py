db13e629ddb1db44466c24600d78a962894d4317
Paulina Koch <kopalinka@wp.pl>
2014-07-18 10:49:34 +0100

    Adding new levels and testing.

--- game/views.py
+++ game/views.py
@@ -121,16 +121,21 @@ def level(request, level):
     blocks = lvl.blocks.order_by('id')
     attempt = None
     lesson = None
-    if lvl.default == 1:
-        lesson = 'description_level' + str(level)
-        hint = 'hint_level' + str(level)
-    else:
+
+    lesson = 'description_level' + str(level)
+    hint = 'hint_level' + str(level)
+    
+    try:
+        lessonCall = getattr(messages, lesson)
+        hintCall = getattr(messages, hint)
+    except AttributeError:
         lesson = 'description_level_default'
         hint = 'hint_level_default'
-    messageCall = getattr(messages, lesson)
-    lesson = mark_safe(messageCall())
-    messageCall = getattr(messages, hint)
-    hint = mark_safe(messageCall())
+        lessonCall = getattr(messages, lesson)
+        hintCall = getattr(messages, hint)
+
+    lesson = mark_safe(lessonCall())
+    hint = mark_safe(hintCall())
 
     #FIXME: figure out how to check for all this better
     loggedInAsStudent = False
