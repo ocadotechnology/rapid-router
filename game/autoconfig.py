390d7ab191e0f08d1fda38659dc504e122a5a3c5
Joseph Zammit <josephezammit@gmail.com>
2014-08-06 11:29:24 +0100

    Added autoconfig

---
+++ game/autoconfig.py
@@ -0,0 +1,17 @@
+'''Game autoconfig'''
+
+SETTINGS = {
+    'INSTALLED_APPS': [
+        'game',
+        'portal',
+        'django.contrib.admin',
+        'django.contrib.admindocs',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'nuit',
+        'rest_framework',
+    ],
+}
