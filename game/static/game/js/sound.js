c8a831073189adda2052ee7227c6a52e93c6aa25
Robert Brignull <robert.brignull@ocado.com>
2014-08-26 10:28:11 +0100

    Second attempt at sound.

--- game/static/game/js/sound.js
+++ game/static/game/js/sound.js
@@ -2,101 +2,67 @@ var ocargo = ocargo || {};
 
 ocargo.sound = {};
 
-function Sound(source, initialVolume, loop) {
-    var audio = new Audio(source);
-    audio.volume = initialVolume;
+ocargo.sound.soundNodes = [];
 
-    audio.loop = loop;
-
-    // A fallback, incase audio.loop doesn't work,
-    // which apparently it doesn't in some browsers
-    if (loop) {
-        audio.onended = function() {
-            audio.src = audio.src;
-            audio.play();
-        }
-    }
-
-    this.play = function() {
-        if (audio.duration) {
-            if (audio.ended) {
-                audio.src = audio.src;
-            }
-            else {
-                audio.currentTime = 0;
-            }
-
-            audio.play();
-        }
-    };
-
-    this.pause = function() {
-        audio.pause();
-    }
-
-    this.setVolume = function(volume) {
-        audio.volume = volume;
-    };
-}
-
-ocargo.sound.startingSound = new Sound("/static/game/sound/starting.mp3", 1.0, false);
-ocargo.sound.engineSound = new Sound("/static/game/sound/moving.wav", 1.0, true);
-ocargo.sound.deliverySound = new Sound("/static/game/sound/delivery.mp3", 1.0, false);
-ocargo.sound.winSound = new Sound("/static/game/sound/win.mp3", 1.0, false);
-ocargo.sound.failureSound = new Sound("/static/game/sound/failure.mp3", 1.0, false);
-ocargo.sound.crashSound = new Sound("/static/game/sound/crash.mp3", 1.0, false);
-ocargo.sound.tensionSound = new Sound("/static/game/sound/tension.mp3", 1.0, false);
+$(function() {
+    ocargo.sound.soundNodes.push(document.getElementById('startingSound'));
+    ocargo.sound.soundNodes.push(document.getElementById('engineSound'));
+    ocargo.sound.soundNodes.push(document.getElementById('deliverySound'));
+    ocargo.sound.soundNodes.push(document.getElementById('winSound'));
+    ocargo.sound.soundNodes.push(document.getElementById('failureSound'));
+    ocargo.sound.soundNodes.push(document.getElementById('crashSound'));
+    ocargo.sound.soundNodes.push(document.getElementById('tensionSound'));
+    
+    document.getElementById('engineSound').loop = true;
+});
 
 ocargo.sound.starting = function() {
-    ocargo.sound.startingSound.play();
+    document.getElementById('startingSound').cloneNode(true).play();
 };
 
 ocargo.sound.start_engine = function() {
-    ocargo.sound.engineSound.play();
+    document.getElementById('engineSound').play();
 };
 
 ocargo.sound.stop_engine = function() {
-    ocargo.sound.engineSound.pause();
+    document.getElementById('engineSound').pause();
 };
 
 ocargo.sound.delivery = function() {
-    ocargo.sound.deliverySound.play();
+    document.getElementById('deliverySound').cloneNode(true).play();
 };
 
 ocargo.sound.win = function() {
-    ocargo.sound.winSound.play();
+    document.getElementById('winSound').cloneNode(true).play();
 };
 
 ocargo.sound.failure = function() {
-    ocargo.sound.failureSound.play();
+    document.getElementById('failureSound').cloneNode(true).play();
 };
 
 ocargo.sound.crash = function() {
-    ocargo.sound.crashSound.play();
+    document.getElementById('crashSound').cloneNode(true).play();
 };
 
 ocargo.sound.tension = function() {
-    ocargo.sound.tensionSound.play();
+    document.getElementById('tensionSound').cloneNode(true).play();
 };
 
 ocargo.sound.setAllVolumes = function(volume) {
-    ocargo.sound.startingSound.setVolume(volume);
-    ocargo.sound.movingSound.setVolume(volume);
-    ocargo.sound.turningSound.setVolume(volume);
-    ocargo.sound.winSound.setVolume(volume);
-    ocargo.sound.failureSound.setVolume(volume);
-    ocargo.sound.tensionSound.setVolume(volume);
+    for (var i in ocargo.sound.soundNodes) {
+        ocargo.sound.soundNodes[i].volume = volume;
+    }
 };
 
 ocargo.sound.mute = function() {
     this.playAudioBackup = Blockly.SOUNDS_;
     Blockly.SOUNDS_ = {};
     ocargo.sound.setAllVolumes(0);
-    $.cookie("muted", true);
+    $.cookie("muted", 'true');
 };
 
 ocargo.sound.unmute = function() {
     Blockly.SOUNDS_ = this.playAudioBackup;
     ocargo.sound.setAllVolumes(1.0);
-    $.cookie("muted", false);
+    $.cookie("muted", 'false');
 };
