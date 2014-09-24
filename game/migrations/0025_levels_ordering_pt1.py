cd671a2e3cce5eb7aaf405a17d31b9f0dc9ef7d8
Paulina Koch <kopalinka@wp.pl>
2014-09-24 12:29:49 +0100

    Added Introduction to Python levels.

--- game/migrations/0025_levels_ordering_pt1.py
+++ game/migrations/0025_levels_ordering_pt1.py
@@ -244,6 +244,120 @@ def fix_levels(apps, schema_editor):
         set_blocks(level66, json.loads('[{"type":"move_forwards"},{"type":"turn_left"},{"type":"turn_right"},{"type":"controls_repeat"},{"type":"call_proc"},{"type":"declare_proc"}]'))
         return level66
 
+    def level80():
+        level88 = Level(
+            name='88',
+            default=True,
+            path='[{"coordinate":[1,3],"connectedNodes":[1]},{"coordinate":[2,3],"connectedNodes":[0,2]},{"coordinate":[3,3],"connectedNodes":[1,3]},{"coordinate":[3,4],"connectedNodes":[4,2]},{"coordinate":[3,5],"connectedNodes":[5,3]},{"coordinate":[4,5],"connectedNodes":[4,6]},{"coordinate":[5,5],"connectedNodes":[5]}]',
+            traffic_lights='[]',
+            destinations='[[5,5]]',
+            origin='{"coordinate":[1,3],"direction":"E"}',
+            max_fuel=50,
+            blocklyEnabled=True,
+            pythonEnabled=True,
+            theme=Theme.objects.get(id=3),
+            character=Character.objects.get(id='1'),
+        )
+        level88.save()
+        set_decor(level88, json.loads('[{"x":436,"y":374,"decorName":"pond"},{"x":437,"y":283,"decorName":"pond"},{"x":872,"y":331,"decorName":"tree1"},{"x":720,"y":193,"decorName":"tree1"},{"x":81,"y":623,"decorName":"tree1"},{"x":190,"y":669,"decorName":"tree1"},{"x":25,"y":521,"decorName":"tree1"},{"x":442,"y":590,"decorName":"bush"},{"x":375,"y":591,"decorName":"bush"},{"x":410,"y":628,"decorName":"bush"},{"x":723,"y":73,"decorName":"tree1"},{"x":603,"y":17,"decorName":"tree1"},{"x":862,"y":169,"decorName":"tree1"},{"x":830,"y":14,"decorName":"tree1"},{"x":0,"y":697,"decorName":"tree1"}]'))
+        set_blocks(level88, json.loads('[{"type":"move_forwards"},{"type":"turn_left"},{"type":"turn_right"},{"type":"controls_repeat"},{"type":"call_proc"},{"type":"declare_proc"}]'))
+        return level88
+
+    def level84():
+        level84 = Level(
+            name='84',
+            default=True,
+            path='[{"coordinate":[0,3],"connectedNodes":[1]},{"coordinate":[1,3],"connectedNodes":[0,2]},{"coordinate":[1,4],"connectedNodes":[3,1]},{"coordinate":[2,4],"connectedNodes":[2,4]},{"coordinate":[2,3],"connectedNodes":[3,5]},{"coordinate":[3,3],"connectedNodes":[4,6]},{"coordinate":[3,4],"connectedNodes":[7,5]},{"coordinate":[4,4],"connectedNodes":[6,8]},{"coordinate":[4,3],"connectedNodes":[7,9]},{"coordinate":[5,3],"connectedNodes":[8,10]},{"coordinate":[5,4],"connectedNodes":[11,9]},{"coordinate":[6,4],"connectedNodes":[10,12]},{"coordinate":[6,3],"connectedNodes":[11,13]},{"coordinate":[7,3],"connectedNodes":[12,14]},{"coordinate":[7,4],"connectedNodes":[15,13]},{"coordinate":[8,4],"connectedNodes":[14,16]},{"coordinate":[8,3],"connectedNodes":[15,17]},{"coordinate":[9,3],"connectedNodes":[16]}]',
+            traffic_lights='[]',
+            destinations='[[9,3]]',
+            origin='{"coordinate":[0,3],"direction":"E"}',
+            max_fuel=50,
+            blocklyEnabled=True,
+            pythonEnabled=True,
+            theme=Theme.objects.get(id=2),
+            character=Character.objects.get(id='1'),
+        )
+        level84.save()
+        set_decor(level84, json.loads('[{"x":156,"y":584,"decorName":"tree1"},{"x":181,"y":169,"decorName":"tree2"},{"x":750,"y":225,"decorName":"tree1"},{"x":311,"y":615,"decorName":"tree2"},{"x":225,"y":509,"decorName":"pond"},{"x":37,"y":483,"decorName":"tree2"},{"x":472,"y":487,"decorName":"tree1"},{"x":54,"y":675,"decorName":"tree1"}]'))
+        set_blocks(level84, json.loads('[{"type":"move_forwards"},{"type":"turn_left"},{"type":"turn_right"},{"type":"controls_repeat"},{"type":"call_proc"},{"type":"declare_proc"}]'))
+        return level84
+
+    def level88():
+        level88 = Level(
+            name='88',
+            default=True,
+            path='[{"coordinate":[2,6],"connectedNodes":[1]},{"coordinate":[2,5],"connectedNodes":[0,2]},{"coordinate":[2,4],"connectedNodes":[10,1,3]},{"coordinate":[2,3],"connectedNodes":[2,4]},{"coordinate":[3,3],"connectedNodes":[3,5]},{"coordinate":[4,3],"connectedNodes":[4,6]},{"coordinate":[4,2],"connectedNodes":[5,14,7]},{"coordinate":[4,1],"connectedNodes":[8,6]},{"coordinate":[3,1],"connectedNodes":[9,7]},{"coordinate":[2,1],"connectedNodes":[8]},{"coordinate":[1,4],"connectedNodes":[11,2]},{"coordinate":[0,4],"connectedNodes":[10,12]},{"coordinate":[0,3],"connectedNodes":[11,13]},{"coordinate":[0,2],"connectedNodes":[12]},{"coordinate":[5,2],"connectedNodes":[6,15]},{"coordinate":[6,2],"connectedNodes":[14,16]},{"coordinate":[6,3],"connectedNodes":[17,15]},{"coordinate":[6,4],"connectedNodes":[18,20,16]},{"coordinate":[5,4],"connectedNodes":[19,17]},{"coordinate":[5,5],"connectedNodes":[18]},{"coordinate":[7,4],"connectedNodes":[17,21]},{"coordinate":[8,4],"connectedNodes":[20,22]},{"coordinate":[8,5],"connectedNodes":[23,21]},{"coordinate":[8,6],"connectedNodes":[22]}]',
+            traffic_lights='[{"redDuration":3,"greenDuration":3,"sourceCoordinate":{"x":2,"y":3},"direction":"E","startTime":0,"startingState":"GREEN"}]',
+            destinations='[[2,1]]',
+            origin='{"coordinate":[2,6],"direction":"S"}',
+            max_fuel=50,
+            blocklyEnabled=True,
+            pythonEnabled=True,
+            theme=Theme.objects.get(id=4),
+            character=Character.objects.get(id='1'),
+        )
+        level88.save()
+        set_decor(level88, json.loads('[{"x":0,"y":117,"decorName":"tree2"},{"x":630,"y":484,"decorName":"pond"},{"x":289,"y":458,"decorName":"bush"},{"x":288,"y":395,"decorName":"bush"},{"x":289,"y":525,"decorName":"bush"},{"x":695,"y":355,"decorName":"bush"},{"x":694,"y":152,"decorName":"bush"},{"x":695,"y":214,"decorName":"bush"},{"x":694,"y":285,"decorName":"bush"},{"x":551,"y":152,"decorName":"bush"},{"x":622,"y":152,"decorName":"bush"},{"x":487,"y":152,"decorName":"bush"},{"x":496,"y":286,"decorName":"tree1"}]'))
+        set_blocks(level88, json.loads('[{"type":"move_forwards"},{"type":"turn_left"},{"type":"turn_right"},{"type":"controls_repeat"},{"type":"call_proc"},{"type":"declare_proc"}]'))
+        return level88
+
+    def level90():
+        level90 = Level(
+            name='90',
+            default=True,
+            path='[{"coordinate":[1,6],"connectedNodes":[1]},{"coordinate":[2,6],"connectedNodes":[0,2]},{"coordinate":[2,5],"connectedNodes":[1,3]},{"coordinate":[3,5],"connectedNodes":[2,4]},{"coordinate":[4,5],"connectedNodes":[3,5]},{"coordinate":[4,4],"connectedNodes":[4,6]},{"coordinate":[5,4],"connectedNodes":[5,7]},{"coordinate":[5,3],"connectedNodes":[6,8]},{"coordinate":[6,3],"connectedNodes":[7,9]},{"coordinate":[7,3],"connectedNodes":[8,10]},{"coordinate":[7,2],"connectedNodes":[9,11]},{"coordinate":[8,2],"connectedNodes":[10,12]},{"coordinate":[8,1],"connectedNodes":[13,11]},{"coordinate":[7,1],"connectedNodes":[14,12]},{"coordinate":[6,1],"connectedNodes":[15,13]},{"coordinate":[5,1],"connectedNodes":[16,14]},{"coordinate":[4,1],"connectedNodes":[17,15]},{"coordinate":[3,1],"connectedNodes":[18,16]},{"coordinate":[3,2],"connectedNodes":[19,17]},{"coordinate":[2,2],"connectedNodes":[20,18]},{"coordinate":[2,3],"connectedNodes":[21,19]},{"coordinate":[1,3],"connectedNodes":[20]}]',
+            traffic_lights='[]',
+            destinations='[[1,3]]',
+            origin='{"coordinate":[1,6],"direction":"E"}',
+            max_fuel=50,
+            blocklyEnabled=True,
+            pythonEnabled=True,
+            theme=Theme.objects.get(id=4),
+            character=Character.objects.get(id='1'),
+        )
+        level90.save()
+        set_decor(level90, json.loads('[{"x":607,"y":213,"decorName":"tree2"},{"x":276,"y":357,"decorName":"pond"},{"x":295,"y":594,"decorName":"bush"},{"x":360,"y":594,"decorName":"bush"},{"x":424,"y":592,"decorName":"bush"},{"x":482,"y":592,"decorName":"bush"},{"x":484,"y":532,"decorName":"bush"},{"x":712,"y":393,"decorName":"bush"},{"x":771,"y":391,"decorName":"bush"},{"x":649,"y":394,"decorName":"bush"},{"x":592,"y":395,"decorName":"bush"},{"x":708,"y":186,"decorName":"bush"},{"x":772,"y":334,"decorName":"bush"},{"x":540,"y":528,"decorName":"bush"},{"x":594,"y":458,"decorName":"bush"},{"x":596,"y":524,"decorName":"bush"}]'))
+        set_blocks(level90, json.loads('[{"type":"move_forwards"},{"type":"turn_left"},{"type":"turn_right"},{"type":"controls_repeat"},{"type":"call_proc"},{"type":"declare_proc"}]'))
+        return level90
+
+    def level91():
+        level91 = Level(
+            name='91',
+            default=True,
+            path='[{"coordinate":[1,6],"connectedNodes":[1]},{"coordinate":[2,6],"connectedNodes":[0,2]},{"coordinate":[3,6],"connectedNodes":[1,3]},{"coordinate":[3,5],"connectedNodes":[2,4]},{"coordinate":[3,4],"connectedNodes":[3,5]},{"coordinate":[3,3],"connectedNodes":[6,4]},{"coordinate":[2,3],"connectedNodes":[5,7]},{"coordinate":[2,2],"connectedNodes":[6,8]},{"coordinate":[3,2],"connectedNodes":[7,9]},{"coordinate":[4,2],"connectedNodes":[8,10]},{"coordinate":[5,2],"connectedNodes":[9,11]},{"coordinate":[5,1],"connectedNodes":[10,12]},{"coordinate":[6,1],"connectedNodes":[11,13]},{"coordinate":[6,2],"connectedNodes":[14,12]},{"coordinate":[6,3],"connectedNodes":[15,13]},{"coordinate":[6,4],"connectedNodes":[16,14]},{"coordinate":[6,5],"connectedNodes":[17,15]},{"coordinate":[7,5],"connectedNodes":[16,18]},{"coordinate":[7,6],"connectedNodes":[19,17]},{"coordinate":[6,6],"connectedNodes":[20,18]},{"coordinate":[5,6],"connectedNodes":[21,19]},{"coordinate":[4,6],"connectedNodes":[22,20]},{"coordinate":[4,7],"connectedNodes":[21]}]',
+            traffic_lights='[]',
+            destinations='[[4,7]]',
+            origin='{"coordinate":[1,6],"direction":"E"}',
+            max_fuel=50,
+            blocklyEnabled=True,
+            pythonEnabled=True,
+            theme=Theme.objects.get(id=2),
+            character=Character.objects.get(id='1'),
+        )
+        level91.save()
+        set_decor(level91, json.loads('[{"x":152,"y":498,"decorName":"tree1"},{"x":103,"y":377,"decorName":"tree1"},{"x":782,"y":329,"decorName":"pond"},{"x":551,"y":482,"decorName":"bush"},{"x":554,"y":545,"decorName":"bush"},{"x":551,"y":286,"decorName":"bush"},{"x":551,"y":349,"decorName":"bush"},{"x":550,"y":414,"decorName":"bush"},{"x":732,"y":221,"decorName":"tree2"},{"x":894,"y":437,"decorName":"tree2"},{"x":0,"y":273,"decorName":"tree1"},{"x":0,"y":502,"decorName":"tree1"}]'))
+        set_blocks(level91, json.loads('[{"type":"move_forwards"},{"type":"turn_left"},{"type":"turn_right"},{"type":"controls_repeat"},{"type":"call_proc"},{"type":"declare_proc"}]'))
+        return level91
+
+    def levelGOD():
+        levelGOD = Level(
+            name='GOD',
+            default=True,
+            path='[{"coordinate":[1,6],"connectedNodes":[1]},{"coordinate":[2,6],"connectedNodes":[0,2]},{"coordinate":[2,5],"connectedNodes":[1,3,19]},{"coordinate":[3,5],"connectedNodes":[2,4]},{"coordinate":[4,5],"connectedNodes":[3,5]},{"coordinate":[4,4],"connectedNodes":[4,6]},{"coordinate":[5,4],"connectedNodes":[5,7]},{"coordinate":[6,4],"connectedNodes":[6,14,8]},{"coordinate":[7,4],"connectedNodes":[7,9]},{"coordinate":[7,3],"connectedNodes":[8,10]},{"coordinate":[8,3],"connectedNodes":[9,18,11]},{"coordinate":[8,2],"connectedNodes":[12,10]},{"coordinate":[7,2],"connectedNodes":[11,13]},{"coordinate":[7,1],"connectedNodes":[26,12]},{"coordinate":[6,5],"connectedNodes":[15,7]},{"coordinate":[6,6],"connectedNodes":[16,14]},{"coordinate":[7,6],"connectedNodes":[15,17]},{"coordinate":[8,6],"connectedNodes":[16]},{"coordinate":[9,3],"connectedNodes":[10]},{"coordinate":[2,4],"connectedNodes":[20,2]},{"coordinate":[1,4],"connectedNodes":[19,21]},{"coordinate":[1,3],"connectedNodes":[24,20,22]},{"coordinate":[2,3],"connectedNodes":[21,23]},{"coordinate":[3,3],"connectedNodes":[22]},{"coordinate":[0,3],"connectedNodes":[21,25]},{"coordinate":[0,2],"connectedNodes":[24]},{"coordinate":[6,1],"connectedNodes":[27,13]},{"coordinate":[5,1],"connectedNodes":[26]}]',
+            traffic_lights='[]',
+            destinations='[[5,1]]',
+            origin='{"coordinate":[1,6],"direction":"E"}',
+            max_fuel=50,
+            blocklyEnabled=True,
+            pythonEnabled=True,
+            theme=Theme.objects.get(id=4),
+            character=Character.objects.get(id='1'),
+        )
+        levelGOD.save()
+        set_decor(levelGOD, json.loads('[{"x":739,"y":509,"decorName":"tree2"},{"x":117,"y":162,"decorName":"pond"},{"x":290,"y":458,"decorName":"bush"},{"x":358,"y":457,"decorName":"bush"},{"x":358,"y":397,"decorName":"bush"},{"x":422,"y":352,"decorName":"bush"},{"x":495,"y":352,"decorName":"bush"},{"x":566,"y":353,"decorName":"bush"},{"x":638,"y":354,"decorName":"bush"},{"x":792,"y":380,"decorName":"tree1"},{"x":692,"y":689,"decorName":"bush"},{"x":627,"y":688,"decorName":"bush"},{"x":824,"y":692,"decorName":"bush"},{"x":756,"y":690,"decorName":"bush"}]'))
+        set_blocks(levelGOD, json.loads('[{"type":"move_forwards"},{"type":"turn_left"},{"type":"turn_right"},{"type":"controls_repeat"},{"type":"call_proc"},{"type":"declare_proc"}]'))
+        return levelGOD
+
     # Limited Blocks
     level51 = Level.objects.get(name='55', default=1)
     level59 = Level.objects.get(name='56', default=1)
@@ -270,18 +384,13 @@ def fix_levels(apps, schema_editor):
     level79 = None
 
     # Introduction to Python
-    level80 = None
     level81 = Level.objects.get(name='80', default=1)
     level82 = Level.objects.get(name='81', default=1)
     level83 = Level.objects.get(name='82', default=1)
-    level84 = None
     level85 = Level.objects.get(name='83', default=1)
     level86 = Level.objects.get(name='84', default=1)
     level87 = Level.objects.get(name='85', default=1)
-    level88 = None  # Simple traffic
     level89 = Level.objects.get(name='86')
-    level90 = None  # Simple procedures
-    level91 = None  # procedures
 
     # Python
     level92 = None
@@ -294,11 +403,13 @@ def fix_levels(apps, schema_editor):
     level99 = Level.objects.get(name='105', default=1)
     level100 = None  # Procedures
     level101 = None  # Procedures
-    level102 = None  # Traffic Lights
-    level103 = None  # Spiral
-    level103 = None  # Spiral
-    level104 = None  # Spiral
-    level105 = None
+    level102 = None  # Procedures (multiple)
+    level103 = None  # Procedures (multiple)
+    level104 = None  # Procecuder (nested)
+    level105 = None  # Traffic Lights
+    level106 = None  # Spiral
+    level107 = None  # Spiral
+    level108 = None  # Spiral
 
     # Limited Blocks
     level52 = level52()
@@ -370,6 +481,25 @@ def fix_levels(apps, schema_editor):
     level70.name = '70'
     level71.name = '71'
 
+    # Introduction to Python
+    level80 = level80()
+    level84 = level84()
+    level88 = level88()
+    level90 = level90()
+    level91 = level91()
+
+    level81.next_level = level82
+    level82.next_level = level83
+    level83.next_level = level84
+    level84.next_level = level85
+    level85.next_level = level86
+    level86.next_level = level87
+    level87.next_level = level88
+    level88.next_level = level89
+    level89.next_level = level90
+    level90.next_level = level91
+    level91.next_level = None
+
     level81.name = '81'
     level82.name = '82'
     level83.name = '83'
@@ -378,26 +508,29 @@ def fix_levels(apps, schema_editor):
     level87.name = '87'
     level89.name = '89'
 
+    level81.save()
+    level82.save()
+    level83.save()
+    level84.save()
+    level85.save()
+    level86.save()
+    level87.save()
+    level89.save()
+    level90.save()
+    level91.save()
+
+    # Python
+    level92 = None
     level93.name = '93'
     level94.name = '94'
     level95.name = '95'
     level98.name = '98'
     level99.name = '99'
-
     
-
     level68.save()
     level69.save()
     level70.save()
     level71.save()
-
-    level81.save()
-    level82.save()
-    level83.save()
-    level85.save()
-    level86.save()
-    level87.save()
-    level89.save()
 
     level93.save()
     level94.save()
@@ -408,7 +541,6 @@ def fix_levels(apps, schema_editor):
 
     dead_end = Block.objects.get(type='dead_end')
     LevelBlock.objects.get(level=level59, type=dead_end).delete()
-
 
 
 class Migration(migrations.Migration):
