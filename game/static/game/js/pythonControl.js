a0a5213a313a2f9eae0e17506acf1f294e66ed2d
Anders Schuller <anders.schuller@ocado.com>
2014-10-15 18:34:46 +0100

    Formatted pythonControl.js

--- game/static/game/js/pythonControl.js
+++ game/static/game/js/pythonControl.js
@@ -1,11 +1,11 @@
 var ocargo = ocargo || {};
 
 var DEFAULT_CODE = "import van\n\nv = van.Van()\n";
-var appendCodeToPanel = function(code, panel) {
-	panel.setValue(DEFAULT_CODE + code.replace(/<br\s*[\/]?>/gi, '\n'));
+var appendCodeToPanel = function (code, panel) {
+    panel.setValue(DEFAULT_CODE + code.replace(/<br\s*[\/]?>/gi, '\n'));
 };
 
-ocargo.PythonControl = function() {
+ocargo.PythonControl = function () {
     /***********/
     /** State **/
     /***********/
@@ -17,7 +17,7 @@ ocargo.PythonControl = function() {
     /** Public methods **/
     /********************/
 
-    this.run = function() {
+    this.run = function () {
         ocargo.model.reset(0);
         Sk.failed = false;
 
@@ -26,53 +26,54 @@ ocargo.PythonControl = function() {
             if (!Sk.failed) {
                 ocargo.model.programExecutionEnded();
             }
-        } catch(e) {
+        } catch (e) {
             outf(e.toString() + "\n");
         }
     };
 
-    this.prepare = function() {
+    this.prepare = function () {
         return {
             success: true,
-            program:{run: this.run},
+            program: {run: this.run},
         };
     };
 
-    this.reset = function() {
+    this.reset = function () {
         this.clearCodePanel();
         this.clearConsole();
     };
 
-    this.clearCodePanel = function() {
+    this.clearCodePanel = function () {
         this.setCode(DEFAULT_CODE);
     };
 
-    this.clearConsole = function() {
+    this.clearConsole = function () {
         console.text("");
     };
 
-    this.appendToConsole = function(str) {
+    this.appendToConsole = function (str) {
         console.text(console.text() + str);
     };
 
-    this.setCode = function(code) {
+    this.setCode = function (code) {
         codePanel.setValue(code.replace(/<br\s*[\/]?>/gi, '\n'));
     };
 
-    this.appendCode = function(code) {
-    	appendCodeToPanel(code, codePanel);
+    this.appendCode = function (code) {
+        appendCodeToPanel(code, codePanel);
     };
 
-    this.getCode = function() {
+    this.getCode = function () {
         return codePanel.getValue();
     };
 
-    this.loadPreviousAttempt = function() {
+    this.loadPreviousAttempt = function () {
         function decodeHTML(text) {
             var e = document.createElement('div');
             e.innerHTML = text;
             return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
         }
+
         if (PYTHON_WORKSPACE) {
             this.setCode(PYTHON_WORKSPACE);
         } else {
@@ -85,7 +86,7 @@ ocargo.PythonControl = function() {
         }
     };
 
-    this.teardown = function() {
+    this.teardown = function () {
         if (localStorage && !ANONYMOUS) {
             var text = this.getCode();
             try {
@@ -140,7 +141,7 @@ ocargo.PythonControl = function() {
         return Sk.builtinFiles.files[x];
     }
 
-    function outf (outputText) {
+    function outf(outputText) {
         ocargo.animation.appendAnimation({
             type: 'console',
             text: outputText
@@ -156,7 +157,9 @@ ocargo.PythonControl = function() {
     var codeView = createCodePanel('pythonView');
     codeView.setValue(DEFAULT_CODE);
     codeView.setOption("readOnly", "nocursor");
-    setInterval(function(){appendCodeToPanel(ocargo.blocklyCompiler.workspaceToPython(), codeView);}, 100);
+    setInterval(function () {
+        appendCodeToPanel(ocargo.blocklyCompiler.workspaceToPython(), codeView);
+    }, 100);
 
     // Limit the code so that it stops after 2 seconds
     Sk.execLimit = 2000;
