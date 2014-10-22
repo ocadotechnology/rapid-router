6b7ff2ed4aca7e2919b3e0eb63ddf3bd094d417c
Paul Heideman <paul.heideman@ocado.com>
2014-10-22 19:10:40 +0100

    Added some caching to the level selection screen and changed attempts query to be a single query

--- game/views/level_selection.py
+++ game/views/level_selection.py
@@ -5,44 +5,14 @@ import game.level_management as level_management
 from django.shortcuts import render
 from django.template import RequestContext
 from django.utils.safestring import mark_safe
+from django.db.models import Max
 from game import random_road
 from game.cache import cached_episode
 from game.models import Attempt, Episode
 from level_editor import play_anonymous_level
+from django.core.cache import cache
 
-
-def levels(request):
-    """ Loads a page with all levels listed.
-
-    **Context**
-
-    ``RequestContext``
-    ``episodes``
-
-    **Template:**
-
-    :template:`game/level_selection.html`
-    """
-    def get_level_title(i):
-        title = 'title_level' + str(i)
-        try:
-            titleCall = getattr(messages, title)
-            return mark_safe(titleCall())
-        except AttributeError:
-            return ""
-
-    def get_attempt_score(level):
-        user = request.user
-        if not user.is_anonymous() and hasattr(user.userprofile, 'student'):
-            student = user.userprofile.student
-            attempt = Attempt.objects.filter(level=level, student=student).first()
-
-            if attempt:
-                return attempt.score
-
-    beta_mode = (not request.user.is_anonymous()) and request.get_host().startswith("beta")
-    developer = (not request.user.is_anonymous()) and request.user.userprofile.developer
-    early_access = developer or beta_mode
+def fetch_episode_data_from_database(early_access):
     episode_data = []
     episode = Episode.objects.get(pk=1)
     while episode is not None:
@@ -62,8 +32,7 @@ def levels(request):
             levels.append({
                 "id": level.id,
                 "name": level_name,
-                "title": get_level_title(level_name),
-                "score": get_attempt_score(level)})
+                "title": get_level_title(level_name)})
 
         e = {"id": episode.id,
              "name": episode.name,
@@ -74,6 +43,58 @@ def levels(request):
 
         episode_data.append(e)
         episode = episode.next_episode
+    return episode_data
+
+def fetch_episode_data(early_access):
+    key = "episode_data"
+    if early_access:
+        key = "episode_data_early_access"
+    data = cache.get(key)
+    if data is None:
+        data = fetch_episode_data_from_database(early_access)
+        cache.set(key, data)
+    return data
+
+def get_level_title(i):
+    title = 'title_level' + str(i)
+    try:
+        titleCall = getattr(messages, title)
+        return mark_safe(titleCall())
+    except AttributeError:
+        return ""
+
+def attach_attempts_to_level(attempts, level):
+    level["score"] = attempts.get(level["id"])
+
+def levels(request):
+    """ Loads a page with all levels listed.
+
+    **Context**
+
+    ``RequestContext``
+    ``episodes``
+
+    **Template:**
+
+    :template:`game/level_selection.html`
+    """
+    user = request.user
+    if not user.is_anonymous() and hasattr(user.userprofile, 'student'):
+        attempts = {a["level_id"]: a["best_score"] for a in \
+                Attempt.objects.filter(student=user.userprofile.student).values("level_id").annotate(best_score=Max("score")).all()}
+    else:
+        attempts = {}
+
+    def with_scores(level):
+        attach_attempts_to_level(attempts, level)
+
+    beta_mode = (not request.user.is_anonymous()) and request.get_host().startswith("beta")
+    developer = (not request.user.is_anonymous()) and request.user.userprofile.developer
+    early_access = developer or beta_mode
+    episode_data = fetch_episode_data(early_access)
+    for e in episode_data:
+        for l in e["levels"]:
+            with_scores(l)
 
     owned_level_data = []
     shared_level_data = []
@@ -84,7 +105,7 @@ def levels(request):
             owned_level_data.append({
                 "id": level.id,
                 "title": level.name,
-                "score": get_attempt_score(level)
+                "score": attempts.get(level.id)
             })
 
         for level in shared_levels:
@@ -92,13 +113,14 @@ def levels(request):
                 "id": level.id,
                 "title": level.name,
                 "owner": level.owner.user,
-                "score": get_attempt_score(level)
+                "score": attempts.get(level.id)
             })
 
     context = RequestContext(request, {
         'episodeData': episode_data,
         'owned_levels': owned_level_data,
         'shared_levels': shared_level_data,
+        'scores': attempts
     })
     return render(request, 'game/level_selection.html', context_instance=context)
 
