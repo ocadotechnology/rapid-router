2a3fc0fc4618c68d2bf3899fcc9456b267dea218
Paul Heideman <paul.heideman@ocado.com>
2015-01-19 14:13:52 +0000

    Updated two level model solutions

---
+++ game/migrations/0036_level_score_73.py
@@ -0,0 +1,19 @@
+from django.db import migrations
+from game.level_management import set_decor, set_blocks
+import json
+
+def update_level(apps, schema_editor):
+    Level = apps.get_model('game', 'Level')
+    level73 = Level.objects.get(name='73', default=1)
+    level73.model_solution = '[12]'
+    level73.save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('game', '0035_disable_route_score_level_70'),
+    ]
+
+    operations = [
+        migrations.RunPython(update_level)
+    ]
