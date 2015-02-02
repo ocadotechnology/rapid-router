ee80ade6c6dc2d5dd5daaa8872428f4ff67a7cb8
Paul Heideman <paul.heideman@ocado.com>
2015-02-02 17:38:03 +0000

    Put new episodes live

---
+++ game/migrations/0039_second_episodes_release.py
@@ -0,0 +1,17 @@
+from django.db import migrations
+
+def release_levels(apps, schema_editor):
+    Episode = apps.get_model('game', 'Episode')
+    for episode in Episode.objects.all():
+        episode.in_development = False
+        episode.save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('game', '0038_level_score_40'),
+    ]
+
+    operations = [
+        migrations.RunPython(release_levels)
+    ]
