e39a45919e9e319c9dd7c134d54a83a05045d51a
Chris Brett <chrisbrett665@gmail.com>
2015-02-04 19:52:20 +0000

    really fixed migrations?

--- game/migrations/0040_auto_20150128_2019.py
+++ game/migrations/0040_auto_20150128_2019.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='level',
             name='episode',
-            field=models.ForeignKey(default=-1, to='game.Episode'),
+            field=models.ForeignKey(default=None, blank=True, to='game.Episode', null=True),
             preserve_default=False,
         ),
     ]
