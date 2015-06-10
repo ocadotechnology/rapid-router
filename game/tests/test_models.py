4c2fc98a5a4e634a2d56ee7ed255c216592dcbba
Paweł Krupiński <pawel.krupinski@ocado.com>
2015-06-10 15:48:37 +0100

    Deleted redundant test.

--- game/tests/test_models.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from django.contrib.auth.models import User
-from django.contrib.sessions.models import Session
-from datetime import datetime
-
-class ClassTestCase(TestCase):
-  def test_that_tests_still_work(self):
-    self.assertTrue(True)
