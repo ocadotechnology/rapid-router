4f31578043dda5ecd4a13f7c4fe1a37c5b0edc89
Paweł Krupiński <pawel.krupinski@ocado.com>
2015-06-29 08:56:12 +0100

    Switching test database to file (default is memory) in order to be able to use --keepdb switch (does not rebuild database with each test run) to make tests run faster.

--- game/settings.py
+++ game/settings.py
@@ -124,6 +124,9 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': rel('dbfile'),
+            'TEST': {
+                'NAME': (rel('testdbfile')),
+            }
         }
     }
     CACHES = {
