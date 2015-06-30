212f851d352e78282f2d9116606533f1347b0828
Cherie Pun <cherie.cy.pun@gmail.com>
2015-06-30 13:45:35 +0100

    Changed from ModelSerializer to HyperlinkedModelSerializer

--- game/serializers.py
+++ game/serializers.py
@@ -7,33 +7,26 @@ class WorkspaceSerializer(serializers.ModelSerializer):
         model = Workspace
 
 
-class LevelListSerializer(serializers.ModelSerializer):
-    episode = serializers.HyperlinkedRelatedField(view_name='episode-detail', read_only=True)
-
+class LevelListSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Level
         fields = ('url', '__unicode__', 'name', 'episode', 'default', 'blocklyEnabled', 'pythonEnabled', )
 
 
-class LevelDetailSerializer(serializers.ModelSerializer):
-    episode = serializers.HyperlinkedRelatedField(view_name='episode-detail', read_only=True)
-
+class LevelDetailSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Level
         fields = ('__unicode__', 'name', 'episode', 'default', 'blocklyEnabled', 'pythonEnabled', 'pythonViewEnabled')
 
 
-class EpisodeListSerializer(serializers.ModelSerializer):
-
+class EpisodeListSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Episode
         fields = ('url', '__unicode__', 'name')
 
 
-class EpisodeDetailSerializer(serializers.ModelSerializer):
-    next_episode = serializers.HyperlinkedRelatedField(view_name='episode-detail', read_only=True)
+class EpisodeDetailSerializer(serializers.HyperlinkedModelSerializer):
     levels = serializers.HyperlinkedRelatedField(view_name='level-detail', many=True, read_only=True)
-
     class Meta:
         model = Episode
         fields = ('url', '__unicode__', 'name', 'next_episode', 'levels')
