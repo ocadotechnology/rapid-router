0625676575e0460b8a7327effaaceb61f61311e6
Paul Heideman <paul.heideman@ocado.com>
2015-07-30 14:55:46 +0100

    Fix typo

--- game/static/game/js/pathFinder.js
+++ game/static/game/js/pathFinder.js
@@ -218,7 +218,7 @@ function aStar(origin, destination, nodes) {
 
         var smallestInOpen = 0;
         var smallestInReverse = 0;
-        for (var i = 0; i < openSet.lenght; i++) {
+        for (var i = 0; i < openSet.length; i++) {
             if (reversePriority[nodes.indexOf(openSet[i])] < reversePriority[smallestInReverse]) {
                 smallestInOpen = i;
                 smallestInReverse = nodes.indexOf(openSet[i]);
