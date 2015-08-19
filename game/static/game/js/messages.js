892c1b20de01b08007ea5f9111df87a2262d895f
Paul Heideman <paul.heideman@ocado.com>
2015-08-19 20:14:37 +0100

    Added missing 'that'

--- game/static/game/js/messages.js
+++ game/static/game/js/messages.js
@@ -81,7 +81,7 @@ ocargo.messages = {
 
     outOfFuel : "You ran out of fuel! Try to find a shorter route to the destination.",
     outOfInstructions: "The van ran out of instructions before it reached a destination. " +
-      "Make sure your blocks are connected to the Start block, and " +
+      "Make sure your blocks are connected to the Start block, and that " +
       "there are enough instructions to complete the delivery.",
     throughRedLight: "Uh oh, you just sent the van through a red light! Stick to the Highway " +
         "Code - the van must wait for green.",
