1035c4a4d057634d9a4a89d621aed3365820b1f1
Paul Heideman <paul.heideman@ocado.com>
2015-08-19 17:27:37 +0100

    Added some more sentences for when you run out of instructions. This should fix #620.

--- game/static/game/js/messages.js
+++ game/static/game/js/messages.js
@@ -80,7 +80,9 @@ ocargo.messages = {
     errorTitle: "Error",
 
     outOfFuel : "You ran out of fuel! Try to find a shorter route to the destination.",
-    outOfInstructions: "The van ran out of instructions before it reached a destination.",
+    outOfInstructions: "The van ran out of instructions before it reached a destination. " +
+      "Make sure your blocks are connected to the Start block, and " +
+      "there are enough instructions to complete the delivery.",
     throughRedLight: "Uh oh, you just sent the van through a red light! Stick to the Highway " +
         "Code - the van must wait for green.",
     alreadyDelivered: "You have already delivered to that destination! You must only deliver " +
