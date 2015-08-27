c7734e57414c8a28d1436a9a13eb4a8f8aacdf26
Paweł Krupiński <pawel.krupinski@ocado.com>
2015-08-27 08:00:45 +0100

    #597 Skipping casper test.

--- game/tests/__init__.py
+++ game/tests/__init__.py
@@ -34,4 +34,3 @@
 # copyright notice and these terms. You must not misrepresent the origins of this
 # program; modified versions of the program must be marked as such and not
 # identified as the original program.
-from _test_javascript import *
