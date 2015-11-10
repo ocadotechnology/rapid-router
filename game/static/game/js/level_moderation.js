386bd8238fd6e7545f94547c5c2d89d7f3a41e6b
Paweł Krupiński <pawel.krupinski@ocado.com>
2015-11-10 15:27:12 +0000

    #701 Using ocargo.Saving in level moderation code.

--- game/static/game/js/level_moderation.js
+++ game/static/game/js/level_moderation.js
@@ -42,6 +42,8 @@ function csrfSafeMethod(method) {
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
+var saving = new ocargo.Saving();
+
 var CONFIRMATION_DATA = {
     'deleteLevel': {
         options: {
@@ -49,29 +51,12 @@ var CONFIRMATION_DATA = {
         },
         html: "<p>This student's level will be permanently deleted. Are you sure?</p>",
         confirm: function() {
-            var csrftoken = $.cookie('csrftoken');
-            $.ajax({
-                url : '/rapidrouter/level_moderation/delete/' + levelID + '/',
-                type : 'POST',
-                dataType: 'json',
-                beforeSend: function(xhr, settings) {
-                    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                        xhr.setRequestHeader("X-CSRFToken", csrftoken);
-                    }
+            saving.deleteLevel(levelID,
+                function () {
+                    document.forms["levelModerationForm"].submit();
                 },
-                success: function(json) {
-                    if(json.success) {
-                        document.forms["levelModerationForm"].submit();
-                    }
-                    else {
-                        console.error("Failed to delete student level.");
-                    }
-                },
-                error : function(xhr,errmsg,err) {
-                    console.error(xhr.status + ": " + errmsg + " " + err + " " + xhr.responseText);
-                }
-            });
-        },
+                console.error);
+        }
     }
 };
 
