cb736269b818c7e06801d1eca1bc91f7a8850c83
Paweł Krupiński <pawel.krupinski@ocado.com>
2015-11-11 13:42:31 +0000

    Revert "Call "reset" every time the animation finishes (e.g. pressing "Stop", finishing "Play", etc)"
    This reverts commit 0436ac141b64c90c2862b147ed102f7a5d7441a1.

--- game/static/game/js/game.js
+++ game/static/game/js/game.js
@@ -519,7 +519,6 @@ ocargo.Game.prototype.onPauseControls = function() {
 };
 
 ocargo.Game.prototype.onStopControls = function() {
-    this.reset();
     this.allowCodeChanges();
 
     // TODO make this hidden unless blocks are clear or something...
@@ -653,6 +652,7 @@ ocargo.Game.prototype._setupPlayTab = function () {
 
 ocargo.Game.prototype._setupStopTab = function () {
     this.tabs.stop.setOnChange(function () {
+        this.reset();
         this.onStopControls();
 
         this.selectPreviousTab();
