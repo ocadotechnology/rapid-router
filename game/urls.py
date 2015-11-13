26fc43a752e71dec7bf120739ece8d82446387ca
Paweł Krupiński <pawel.krupinski@ocado.com>
2015-11-13 09:19:51 +0000

    Inlining included urls.

--- game/urls.py
+++ game/urls.py
@@ -49,66 +49,6 @@ from game.views.level import submit_attempt, play_default_level, start_episode, 
     load_list_of_workspaces, save_workspace, delete_workspace, \
     delete_level, play_custom_level, play_custom_level_from_editor
 from game.views.scoreboard import scoreboard
-
-level_editor_urls = [
-    url(r'^$', level_editor, name='level_editor'),
-    url(r'^play_anonymous/(?P<levelId>[0-9]+)/$', play_anonymous_level, name='play_anonymous_level'),
-    url(r'^play_custom/(?P<levelId>[0-9]+)/$', play_custom_level_from_editor,
-        name='play_custom_level_from_editor'),
-
-    url(r'^get/(?P<levelID>[0-9]+)/$', load_level_for_editor, name='load_level_for_editor'),
-    url(r'^delete/(?P<levelId>[0-9]+)/$', delete_level_for_editor, name='delete_level_for_editor'),
-    url(r'^save/$', save_level_for_editor),
-    url(r'^save/(?P<levelId>[0-9]+)/$', save_level_for_editor, name='save_level_for_editor'),
-    url(r'^random/$', generate_random_map_for_editor, name='generate_random_map_for_editor'),
-
-    url(r'^get_sharing_information/(?P<levelID>[0-9]+)/$',
-        get_sharing_information_for_editor, name='get_sharing_information_for_editor'),
-    url(r'^share/(?P<levelID>[0-9]+)/$',
-        share_level_for_editor, name='share_level_for_editor'),
-
-    url(r'^levels/owned/$', owned_levels, name='owned_levels'),
-    url(r'^levels/shared/$', shared_levels, name='owned_levels'),
-]
-
-workspace_urls = [
-    url(r'^load/(?P<workspaceID>[0-9]+)/$', load_workspace, name='load_workspace'),
-    url(r'^load_list/$', load_list_of_workspaces, name='load_list_of_workspaces'),
-    url(r'^save/$', save_workspace, name='save_workspace'),
-    url(r'^save/(?P<workspaceID>[0-9]+)/$', save_workspace, name='save_workspace'),
-    url(r'^delete/(?P<workspaceID>[0-9]+)/$', delete_workspace, name='delete_workspace'),
-]
-
-level_moderation_urls = [
-    url(r'^$', level_moderation, name='level_moderation'),
-    url(r'^class/(?P<class_id>[0-9]+)/$', get_students_for_level_moderation, name='students_for_level_moderation'),
-    url(r'^delete/(?P<levelID>[0-9]+)/$', delete_level, name='delete_level'),
-]
-
-api_urls = [
-    url(r'^$', api_root),
-    url(r'^blocks/$', block_list, name='block-list'),
-    url(r'^blocks/(?P<pk>[0-9]+)/$', block_detail, name='block-detail'),
-    url(r'^characters/$', character_list, name='character-list'),
-    url(r'^characters/(?P<pk>[0-9]+)/$', character_detail, name='character-detail'),
-    url(r'^decors/$', decor_list, name='decor-list'),
-    url(r'^decors/(?P<pk>[0-9]+)/$', decor_detail, name='decor-detail'),
-    url(r'^episodes/$', episode_list, name='episode-list'),
-    url(r'^episodes/(?P<pk>[0-9]+)/$', episode_detail, name='episode-detail'),
-    url(r'^episodes/(?P<pk>[0-9]+)/levels/$', level_for_episode, name='level-for-episode'),
-    url(r'^levels/$', level_list, name='level-list'),
-    url(r'^levels/(?P<pk>[0-9]+)/$', level_detail, name='level-detail'),
-    url(r'^levels/(?P<pk>[0-9]+)/blocks/$', levelblock_for_level, name='levelblock-for-level'),
-    url(r'^levels/(?P<pk>[0-9]+)/decors/$', leveldecor_for_level, name='leveldecor-for-level'),
-    url(r'^levels/(?P<pk>[0-9]+)/map/$', map_for_level, name='map-for-level'),
-    url(r'^levels/(?P<pk>[0-9]+)/mode/$', mode_for_level, name='mode-for-level'),
-    url(r'^levelblocks/(?P<pk>[0-9]+)/$', levelblock_detail, name='levelblock-detail'),
-    url(r'^leveldecors/(?P<pk>[0-9]+)/$', leveldecor_detail, name='leveldecor-detail'),
-    url(r'^maps/$', map_list, name='map-list'),
-    url(r'^themes/$', theme_list, name='theme-list'),
-    url(r'^themes/(?P<pk>[0-9]+)/$', theme_detail, name='theme-detail'),
-]
-
 urlpatterns = patterns(
     '',
     url(r'^$', levels, name='levels'),
@@ -119,10 +59,64 @@ urlpatterns = patterns(
     url(r'^levels/random/([0-9]+)/$', random_level_for_episode, name='random_level_for_episode'),
     url(r'^scoreboard/$', scoreboard, name='scoreboard'),
 
-    url(r'^workspace/', include(workspace_urls)),
-    url(r'^level_moderation/', include(level_moderation_urls)),
-    url(r'^level_editor/', include(level_editor_urls)),
+    url(r'^workspace/', include([
+        url(r'^load/(?P<workspaceID>[0-9]+)/$', load_workspace, name='load_workspace'),
+        url(r'^load_list/$', load_list_of_workspaces, name='load_list_of_workspaces'),
+        url(r'^save/$', save_workspace, name='save_workspace'),
+        url(r'^save/(?P<workspaceID>[0-9]+)/$', save_workspace, name='save_workspace'),
+        url(r'^delete/(?P<workspaceID>[0-9]+)/$', delete_workspace, name='delete_workspace'),
+    ])),
 
-    url(r'^api/', include(api_urls)),
+    url(r'^level_moderation/', include([
+        url(r'^$', level_moderation, name='level_moderation'),
+        url(r'^class/(?P<class_id>[0-9]+)/$', get_students_for_level_moderation, name='students_for_level_moderation'),
+        url(r'^delete/(?P<levelID>[0-9]+)/$', delete_level, name='delete_level'),
+    ])),
+
+    url(r'^level_editor/', include([
+        url(r'^$', level_editor, name='level_editor'),
+        url(r'^play_anonymous/(?P<levelId>[0-9]+)/$', play_anonymous_level, name='play_anonymous_level'),
+        url(r'^play_custom/(?P<levelId>[0-9]+)/$', play_custom_level_from_editor,
+            name='play_custom_level_from_editor'),
+
+        url(r'^get/(?P<levelID>[0-9]+)/$', load_level_for_editor, name='load_level_for_editor'),
+        url(r'^delete/(?P<levelId>[0-9]+)/$', delete_level_for_editor, name='delete_level_for_editor'),
+        url(r'^save/$', save_level_for_editor),
+        url(r'^save/(?P<levelId>[0-9]+)/$', save_level_for_editor, name='save_level_for_editor'),
+        url(r'^random/$', generate_random_map_for_editor, name='generate_random_map_for_editor'),
+
+        url(r'^get_sharing_information/(?P<levelID>[0-9]+)/$',
+            get_sharing_information_for_editor, name='get_sharing_information_for_editor'),
+        url(r'^share/(?P<levelID>[0-9]+)/$',
+            share_level_for_editor, name='share_level_for_editor'),
+
+        url(r'^levels/owned/$', owned_levels, name='owned_levels'),
+        url(r'^levels/shared/$', shared_levels, name='owned_levels'),
+    ])),
+
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include([
+        url(r'^$', api_root),
+        url(r'^blocks/$', block_list, name='block-list'),
+        url(r'^blocks/(?P<pk>[0-9]+)/$', block_detail, name='block-detail'),
+        url(r'^characters/$', character_list, name='character-list'),
+        url(r'^characters/(?P<pk>[0-9]+)/$', character_detail, name='character-detail'),
+        url(r'^decors/$', decor_list, name='decor-list'),
+        url(r'^decors/(?P<pk>[0-9]+)/$', decor_detail, name='decor-detail'),
+        url(r'^episodes/$', episode_list, name='episode-list'),
+        url(r'^episodes/(?P<pk>[0-9]+)/$', episode_detail, name='episode-detail'),
+        url(r'^episodes/(?P<pk>[0-9]+)/levels/$', level_for_episode, name='level-for-episode'),
+        url(r'^levels/$', level_list, name='level-list'),
+        url(r'^levels/(?P<pk>[0-9]+)/$', level_detail, name='level-detail'),
+        url(r'^levels/(?P<pk>[0-9]+)/blocks/$', levelblock_for_level, name='levelblock-for-level'),
+        url(r'^levels/(?P<pk>[0-9]+)/decors/$', leveldecor_for_level, name='leveldecor-for-level'),
+        url(r'^levels/(?P<pk>[0-9]+)/map/$', map_for_level, name='map-for-level'),
+        url(r'^levels/(?P<pk>[0-9]+)/mode/$', mode_for_level, name='mode-for-level'),
+        url(r'^levelblocks/(?P<pk>[0-9]+)/$', levelblock_detail, name='levelblock-detail'),
+        url(r'^leveldecors/(?P<pk>[0-9]+)/$', leveldecor_detail, name='leveldecor-detail'),
+        url(r'^maps/$', map_list, name='map-list'),
+        url(r'^themes/$', theme_list, name='theme-list'),
+        url(r'^themes/(?P<pk>[0-9]+)/$', theme_detail, name='theme-detail'),
+    ])),
 )
+
