a8a9f711302c5e0d8ab3fa0b9a83c67421baa092
Oliver Jeeves <oj@spoonmeiser.org.uk>
2016-02-03 19:55:27 +0000

    Remove references to portal.UserProfile
    Instead have models reference User directly

--- game/models.py
+++ game/models.py
@@ -37,7 +37,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from portal.models import UserProfile, Student
+from portal.models import Student
 
 
 class Block(models.Model):
@@ -127,7 +127,7 @@ class Level(models.Model):
     origin = models.CharField(max_length=50, default='[]')
     destinations = models.CharField(max_length=50, default='[[]]')
     default = models.BooleanField(default=False)
-    owner = models.ForeignKey(UserProfile, related_name='levels', blank=True, null=True)
+    owner = models.ForeignKey(User, related_name='levels', blank=True, null=True)
     fuel_gauge = models.BooleanField(default=True)
     max_fuel = models.IntegerField(default=50)
     direct_drive = models.BooleanField(default=False)
@@ -163,7 +163,7 @@ class LevelDecor(models.Model):
 
 class Workspace(models.Model):
     name = models.CharField(max_length=200)
-    owner = models.ForeignKey(UserProfile, related_name='workspaces', blank=True, null=True)
+    owner = models.ForeignKey(User, related_name='workspaces', blank=True, null=True)
     contents = models.TextField(default="")
     python_contents = models.TextField(default="")
     blockly_enabled = models.BooleanField(default=False)
