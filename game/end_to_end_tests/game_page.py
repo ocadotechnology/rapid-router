656f004396386732df5a38e27db8374421511ef7
Joshua Blake <joshua.blake@ocado.com>
2016-09-06 10:36:53 +0100

    Make run_program work on all levels, even without algorithm score

--- game/end_to_end_tests/game_page.py
+++ game/end_to_end_tests/game_page.py
@@ -97,7 +97,7 @@ class GamePage(BasePage):
 
         assert_that(image.get_attribute("opacity"), equal_to("1"))
 
-    def run_program(self, wait_for_element_id="algorithmScore"):
+    def run_program(self, wait_for_element_id="modal-content"):
         self.browser.find_element_by_id("fast_tab").click()
 
         try:
