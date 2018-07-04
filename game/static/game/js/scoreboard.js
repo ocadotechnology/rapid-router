888e08efd589af95c959bdecd1ced336e16c4579
Quentin Sollet <quentin.sollet@ocado.com>
2018-07-04 13:12:37 +0100

    fixed error showed in codeclimate

--- game/static/game/js/scoreboard.js
+++ game/static/game/js/scoreboard.js
@@ -39,17 +39,12 @@ $(':checkbox').on('change', function() {
     $(this).closest('li').toggleClass('checked', $(this).is(':checked'))
 })
 
-$('.all-class > label').on('change', function() {
+$('.all-class > label, .all-level').on('change', function() {
     var check = $(this).find('input').is(':checked')
     $(this).closest('ul').find(':checkbox').prop('checked', check).closest('li').toggleClass('checked', check)
 })
 
-$('.all-level').on('change', function(e) {
-    var check = $(this).find('input').is(':checked')
-    $(this).closest('ul').find(':checkbox').prop('checked', check).closest('li').toggleClass('checked', check)
-})
-
-$('.episode').on('change', function(e) {
+$('.episode').on('change', function() {
     var check = $(this).find('input').is(':checked')
     var first = $(this).attr('data-first')
     var last = $(this).attr('data-last')
@@ -58,7 +53,7 @@ $('.episode').on('change', function(e) {
     }
 })
 
-$('.expender').on('click', function(e) {
+$('.expender').on('click', function() {
     var el = $(this).closest('li')
     var first = el.attr('data-first')
     var last = el.attr('data-last')
