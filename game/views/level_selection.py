794f5f3093a855e819b169ea260ddcbbbb969631
faucomte97 <f.aucomte@hotmail.co.uk>
2018-12-19 14:00:23 +0000

    Fixes scoreboard loading issue

--- game/views/level_selection.py
+++ game/views/level_selection.py
@@ -83,6 +83,7 @@ def fetch_episode_data_from_database(early_access):
                 "title": get_level_title(level_name)})
 
         e = {"id": episode.id,
+             "name": messages.get_episode_title(episode.id),
              "levels": levels,
              "first_level": minName,
              "last_level": maxName,
