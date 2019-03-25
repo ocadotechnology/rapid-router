6cc144efa760801076b0faf207ae345a40117644
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2019-03-25 10:24:13 +0000

    refactor: Make URLs consistent with deployed project (#1023)
    * Make URLs consistent with other projects

--- test_settings.py
+++ test_settings.py
@@ -1,4 +1,3 @@
-import os
 from selenium import webdriver
 
 SELENIUM_WEBDRIVERS = {
@@ -19,11 +18,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
     },
 }
+
 INSTALLED_APPS = ['game']
 PIPELINE_ENABLED = False
-ROOT_URLCONF = 'django_autoconfig.autourlconf'
-SECRET_KEY = 'test'
+ROOT_URLCONF = 'example_project.example_project.urls'
 STATIC_ROOT = 'example_project/example_project/static/'
+SECRET_KEY = 'test'
 
 from django_autoconfig.autoconfig import configure_settings
 configure_settings(globals())
