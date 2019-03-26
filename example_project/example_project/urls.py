e26e28c5b6584d70a0db5cd2a826478ce48281a1
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-03-26 13:49:59 +0000

    Black formatting

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns(
-    '',
-    url(r'^', include('portal.urls')),
-    url(r'^administration/', include(admin.site.urls)),
-    url(r'^rapidrouter/', include('game.urls')),
-    url(r'^aimmo/', include('aimmo.urls')),
+    "",
+    url(r"^", include("portal.urls")),
+    url(r"^administration/", include(admin.site.urls)),
+    url(r"^rapidrouter/", include("game.urls")),
+    url(r"^aimmo/", include("aimmo.urls")),
 )
--- test_settings.py
+++ test_settings.py
@@ -1,29 +1,18 @@
 from selenium import webdriver
 
 SELENIUM_WEBDRIVERS = {
-    'default': {
-        'callable': webdriver.Chrome,
-        'args': (),
-        'kwargs': {},
-    },
-    'chrome': {
-        'callable': webdriver.Firefox,
-        'args': (),
-        'kwargs': {},
-    },
+    "default": {"callable": webdriver.Chrome, "args": (), "kwargs": {}},
+    "chrome": {"callable": webdriver.Firefox, "args": (), "kwargs": {}},
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-    },
-}
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
 
-INSTALLED_APPS = ['game']
+INSTALLED_APPS = ["game"]
 PIPELINE_ENABLED = False
-ROOT_URLCONF = 'example_project.example_project.urls'
-STATIC_ROOT = 'example_project/example_project/static/'
-SECRET_KEY = 'test'
+ROOT_URLCONF = "example_project.example_project.urls"
+STATIC_ROOT = "example_project/example_project/static/"
+SECRET_KEY = "test"
 
 from django_autoconfig.autoconfig import configure_settings
+
 configure_settings(globals())
