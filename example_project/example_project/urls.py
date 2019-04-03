37559a3d677f162f56a7c0e159767ec69093c4ae
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-04-03 09:51:09 +0100

    Using a list instead of URL patterns

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import include, patterns, url
+from aimmo import urls as aimmo_urls
+from django.conf.urls import include, url
 from django.contrib import admin
+from game import urls as game_urls
+from portal import urls as portal_urls
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    url(r'^', include('portal.urls')),
+urlpatterns = [
+    url(r'^', include(portal_urls)),
     url(r'^administration/', include(admin.site.urls)),
-    url(r'^rapidrouter/', include('game.urls')),
-    url(r'^aimmo/', include('aimmo.urls')),
-)
+    url(r'^rapidrouter/', include(game_urls)),
+    url(r'^aimmo/', include(aimmo_urls)),
+]
