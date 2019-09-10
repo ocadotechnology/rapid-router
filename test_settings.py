d7b59c7397b7f28adeebdb5f19c1ccd5e3635ef0
Niket Shah <masterniket@gmail.com>
2019-09-10 10:42:30 +0100

    Remove not needed settings in test_settings
    Signed-off-by: Niket Shah <masterniket@gmail.com>

--- test_settings.py
+++ test_settings.py
@@ -10,22 +10,13 @@ DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
 
 INSTALLED_APPS = ["game"]
 PIPELINE_ENABLED = False
-# STATICFILES_STORAGE='pipeline.storage.PipelineStorage'
 
 ROOT_URLCONF = "example_project.example_project.urls"
 STATIC_ROOT = os.path.join(
-    os.path.dirname(__file__), "example_project/example_project", "static/"
+    os.path.dirname(__file__), "example_project/example_project", "static"
 )
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-    "pipeline.finders.PipelineFinder",
-]
-STATIC_URL = "/static/"
 SECRET_KEY = "test"
 
-DEBUG = True
-STATICFILES_STORAGE = "pipeline.storage.PipelineStorage"
 
 from django_autoconfig.autoconfig import configure_settings
 
