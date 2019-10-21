8b24080dd17e10dddb33cd3168dd9f943666db1e
Niket Shah <masterniket@gmail.com>
2019-10-21 11:45:19 +0100

    Revert "always log on"
    This reverts commit abaac8c568ce27c72bb93b90572c10b95e66c91d.

--- game/end_to_end_tests/base_game_test.py
+++ game/end_to_end_tests/base_game_test.py
@@ -217,27 +217,27 @@ class BaseGameTest(SeleniumTestCase):
         return workspace_id
 
     def login_once(self, the_test=False):
-        # if not BaseGameTest.already_logged_on:
-        if the_test:
-            print("got inside of I need to log on part")
-        email, password = signup_teacher_directly()
-        create_organisation_directly(email)
-        klass, name, access_code = create_class_directly(email)
-        create_school_student_directly(access_code)
-        login_page = self.go_to_homepage().go_to_login_page()
-        if the_test:
-            print("BEFORE LOGIN PAGE")
-            print(self.selenium.page_source)
-        login_page.login(email, password)
-        if the_test:
-            print("AFTER LOGIN PAGE")
-            print(self.selenium.page_source)
-        email = email
-        BaseGameTest.user_profile = UserProfile.objects.get(user__email=email)
+        if not BaseGameTest.already_logged_on:
+            if the_test:
+                print("got inside of I need to log on part")
+            email, password = signup_teacher_directly()
+            create_organisation_directly(email)
+            klass, name, access_code = create_class_directly(email)
+            create_school_student_directly(access_code)
+            login_page = self.go_to_homepage().go_to_login_page()
+            if the_test:
+                print("BEFORE LOGIN PAGE")
+                print(self.selenium.page_source)
+            login_page.login(email, password)
+            if the_test:
+                print("AFTER LOGIN PAGE")
+                print(self.selenium.page_source)
+            email = email
+            BaseGameTest.user_profile = UserProfile.objects.get(user__email=email)
 
-            # BaseGameTest.already_logged_on = True
-        # else:
-        #     print("I am already logged in with {}".format(BaseGameTest.user_profile))
+            BaseGameTest.already_logged_on = True
+        else:
+            print("I am already logged in with {}".format(BaseGameTest.user_profile))
 
         return BaseGameTest.user_profile
 
