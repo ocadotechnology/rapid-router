4cb9a0076d77372d7e8f1e3286eca345580dbf07
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-12-12 13:53:43 +0000

    added wait_seconds to wait_for_element_to_be_invisible

--- game/end_to_end_tests/game_page.py
+++ game/end_to_end_tests/game_page.py
@@ -65,6 +65,7 @@ class GamePage(BasePage):
     def dismiss_dialog(self, button_id):
         self.wait_for_element_to_be_clickable((By.ID, button_id), wait_seconds=15)
         self.browser.find_element_by_id(button_id).click()
+        self.wait_for_element_to_be_invisible((By.ID, button_id), wait_seconds=10)
 
     def load_solution(self, workspace_id):
         self.browser.find_element_by_id("load_tab").click()
