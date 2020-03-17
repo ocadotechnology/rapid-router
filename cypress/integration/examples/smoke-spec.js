45a7c65e7f5da692e7f78c1389ce21cf4a95cb2e
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-03-17 10:19:57 +0000

    Graft test files

--- cypress/integration/examples/smoke-spec.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-
-import { smokeTest } from './smoke'
-
-it('does not smoke', () => {
-  smokeTest()
-})
--- cypress/integration/examples/viewport.spec.js
+++
@@ -1,8 +0,0 @@
-import { smokeTest } from './smoke'
-
-Cypress._.each(['macbook-15', 'iphone-6'], viewport => {
-  it(`works on ${viewport}`, () => {
-    cy.viewport(viewport)
-    smokeTest()
-  })
-})
