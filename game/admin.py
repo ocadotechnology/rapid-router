bbf325aa502a9ae36a1761c7de486c251e7d8235
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-04-08 15:56:38 +0100

    fix: Improve admin pages by reducing load

--- game/admin.py
+++ game/admin.py
@@ -39,10 +39,15 @@ from game.models import Level, Block, Episode, Workspace
 
 
 class LevelAdmin(admin.ModelAdmin):
+    raw_id_fields = ["next_level"]
     readonly_fields = ["owner"]
 
 
+class WorkspaceAdmin(admin.ModelAdmin):
+    raw_id_fields = ["owner"]
+
+
 admin.site.register(Level, LevelAdmin)
-admin.site.register(Workspace)
+admin.site.register(Workspace, WorkspaceAdmin)
 admin.site.register(Episode)
 admin.site.register(Block)
