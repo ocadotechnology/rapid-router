4acd87d93878d8c8f63ba1bf35ba7aa5ea840ecd
Seb Palmer <s.palmer1@ocado.com>
2020-09-25 16:42:06 +0100

    fix: Updated migration for solution for level 50

--- game/migrations/0072_level_50_solution.py
+++ game/migrations/0072_level_50_solution.py
@@ -36,15 +36,19 @@
 # identified as the original program.
 from django.db import migrations
 
-
 def update_level(apps, schema_editor):
+
     Level = apps.get_model("game", "Level")
     level50 = Level.objects.get(name="50", default=1)
     level50.model_solution = "[11]"
     level50.save()
 
+def dummy_reverse(apps, schema_editor):
+    """It's not possible to reverse this data migration
+    but we want to allow Django to reverse previous migrations.
+    """
+    pass
 
 class Migration(migrations.Migration):
     dependencies = [("game", "0071_use_common_models")]
-
-    operations = [migrations.RunPython(update_level)]
+    operations = [migrations.RunPython(update_level, reverse_code=dummy_reverse)]
