1580377127083d48c0bcef467daec583e9144021
Seb Palmer <s.palmer1@ocado.com>
2020-09-30 15:54:09 +0100

    chore: formatting

--- game/migrations/0072_level_50_solution.py
+++ game/migrations/0072_level_50_solution.py
@@ -36,6 +36,7 @@
 # identified as the original program.
 from django.db import migrations
 
+
 def update_level(apps, schema_editor):
 
     Level = apps.get_model("game", "Level")
@@ -43,11 +44,13 @@ def update_level(apps, schema_editor):
     level50.model_solution = "[11]"
     level50.save()
 
+
 def dummy_reverse(apps, schema_editor):
     """It's not possible to reverse this data migration
     but we want to allow Django to reverse previous migrations.
     """
     pass
+
 
 class Migration(migrations.Migration):
     dependencies = [("game", "0071_use_common_models")]
