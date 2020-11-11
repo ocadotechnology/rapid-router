f76d726144ce98f8ecaef215ec9fd0abf5eeffb4
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-11-11 13:29:44 +0000

    Simplify admin urls + Black

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -1,6 +1,7 @@
 from aimmo import urls as aimmo_urls
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
 from portal import urls as portal_urls
 
 from game import urls as game_urls
@@ -9,7 +10,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r"^", include(portal_urls)),
-    url(r"^administration/", include((admin.site.urls[0], "admin"), namespace="admin")),
+    path("admin/", admin.site.urls),
     url(r"^rapidrouter/", include(game_urls)),
     url(r"^aimmo/", include(aimmo_urls)),
 ]
