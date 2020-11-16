c9b9bdc787b9c4a68bc40297090d35cefb100d89
Razvan Mahu <razvan.mahu@ocado.com>
2020-11-16 14:53:53 +0000

    try a few args for chrome-headless

--- rapid_router_test_settings.py
+++ rapid_router_test_settings.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
 import os
 
+headless_chrome_options = webdriver.ChromeOptions()
+headless_chrome_options.add_argument("--headless")
+headless_chrome_options.add_argument("--no-sandbox")
+
 SELENIUM_WEBDRIVERS = {
     "default": {"callable": webdriver.Chrome, "args": (), "kwargs": {}},
     "chrome": {"callable": webdriver.Firefox, "args": (), "kwargs": {}},
+    "chrome-headless": {"callable": webdriver.Chrome, "args": (), "kwargs": {
+        'options': headless_chrome_options
+    }},
 }
 
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
