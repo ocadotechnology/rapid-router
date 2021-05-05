a6274ac3818fb338d19e916aff0857acc74cb802
Dioni Zhong <dioni.zhong@ocado.com>
2021-05-05 11:44:55 +0100

    put back function, or the tests would time out

--- game/static/game/js/event.js
+++ game/static/game/js/event.js
@@ -41,6 +41,9 @@ var ocargo = ocargo || {};
 
 ocargo.Event = function() {};
 
+ocargo.Event.prototype.sendEvent = function(eventType, details) {
+};
+
 $(document).ready(function() {
   ocargo.event = new ocargo.Event();
 });
