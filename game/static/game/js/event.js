36794916c2cd58a3de09fb1a64a416416e3cc4d9
Dioni Zhong <dioni.zhong@ocado.com>
2021-05-05 11:59:29 +0100

    fix: update jquery.cookie to js.cookie (#1177)
    * fix: update jquery.cookie to js.cookie
    
    * put back function, or the tests would time out

--- game/static/game/js/event.js
+++ game/static/game/js/event.js
@@ -42,17 +42,6 @@ var ocargo = ocargo || {};
 ocargo.Event = function() {};
 
 ocargo.Event.prototype.sendEvent = function(eventType, details) {
-  //var csrftoken = $.cookie('csrftoken');
-  //$.ajax({
-  //   url: '/reports/event',
-  //   type: 'POST',
-  //   async: true,
-  //   dataType: 'json',
-  //   beforeSend: function(xhr, settings) {
-  //     xhr.setRequestHeader("X-CSRFToken", csrftoken);
-  //   },
-  //   data: JSON.stringify({ app: "RapidRouter", eventType: eventType, details: details }),
-  // });
 };
 
 $(document).ready(function() {
