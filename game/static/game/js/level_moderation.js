880dd98b8f86942f45b307d7e7c0c09da0108e7c
Seb Palmer <s.palmer1@ocado.com>
2021-09-10 18:40:00 +0100

    fix: 1099, change javascript in level_moderation and include modal in page template

--- game/static/game/js/level_moderation.js
+++ game/static/game/js/level_moderation.js
@@ -46,23 +46,7 @@ function csrfSafeMethod(method) {
 
 var saving = new ocargo.Saving();
 
-var CONFIRMATION_DATA = {
-    'deleteLevel': {
-        options: {
-            title: gettext('Delete level'),
-        },
-        html: '<p>' + gettext("This student's level will be permanently deleted. Are you sure?") + '</p>',
-        confirm: function() {
-            saving.deleteLevel(levelID,
-                function () {
-                    document.forms["levelModerationForm"].submit();
-                },
-                console.error);
-        }
-    }
-};
-
-function deleteLevel(e) {
+function deleteLevel() {
     saving.deleteLevel(levelID,
         function () {
             document.forms["levelModerationForm"].submit();
@@ -72,29 +56,26 @@ function deleteLevel(e) {
 
 function showPopupConfirmation(title, text, confirm_handler) {
     var popup = $(".popup-wrapper");
-    $(".popup-box__title").text('Delete level');
-    $(".popup-box__msg").append('<p>' + gettext("This student's level will be permanently deleted. Are you sure?") + '</p>');
-    console.log($("#confirm_button"));
-    $("#confirm_button").click(deleteLevel);
+    $(".popup-box__title").text(title);
+    $(".popup-box__msg").append(text);
+    $("#confirm_button").click(confirm_handler);
 
     popup.addClass("popup--fade");
 }
 
-function openConfirmationBox(type) {
-    var data = CONFIRMATION_DATA[type];
+function confirmDelete() {
+    console.log(levelID);
+    var title = 'Delete level';
+    var text = '<p>' + gettext("This student's level will be permanently deleted. Are you sure?") + '</p>';
 
-    var title = data.options.title;
-    var text = data.html;
-    var confirm_handler = data.confirm;
-
-    showPopupConfirmation(title, text, data.confirm);
-
+    showPopupConfirmation(title, text, deleteLevel);
 }
+
 $(document).ready(function() {
 	$(".delete").click(function() {
 
 	  	levelID = this.getAttribute('value');
-	  	openConfirmationBox('deleteLevel');
+	  	confirmDelete();
 	});
 
 	$('.play').click(function() {
