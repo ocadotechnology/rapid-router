1707f37db2ccfcfe9ac49b0020d0fce6bf6e1f32
Seb Palmer <s.palmer1@ocado.com>
2021-11-04 14:36:56 +0000

    fix: deleting workspace now makes the name disappear

--- game/static/game/js/saving.js
+++ game/static/game/js/saving.js
@@ -92,6 +92,7 @@ ocargo.Saving.prototype.deleteWorkspace = function (id, callback) {
                 }
             },
             success: function (json) {
+                $("#loadWorkspaceTable > tr[selected]").remove();
                 callback(null, json);
             },
             error: function (xhr, errmsg, err) {
