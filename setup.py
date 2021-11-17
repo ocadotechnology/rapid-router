3942d9f9fe6d86dd95f835c8cb795972fcab084f
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-11-17 11:16:12 +0000

    Python 3.6 ro 3.7

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Framework :: Django",
     ],
     version=version,
