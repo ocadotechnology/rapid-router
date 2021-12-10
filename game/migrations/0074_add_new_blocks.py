a5f854346c153af57bf68ffeedc08406f1a781c3
Razvan Mahu <razvan.mahu@ocado.com>
2021-12-10 12:46:53 +0000

    add migration

---
+++ game/migrations/0074_add_new_blocks.py
@@ -0,0 +1,42 @@
+from django.db import migrations
+
+
+def add_new_blocks(apps, schema_editor):
+    Block = apps.get_model("game", "Block")
+
+    block1 = Block(type="variables_numeric_set")
+    block2 = Block(type="variables_increment")
+    block3 = Block(type="variables_get")
+    block4 = Block(type="math_number")
+    block5 = Block(type="logic_compare")
+
+    block1.save()
+    block2.save()
+    block3.save()
+    block4.save()
+    block5.save()
+
+
+def remove_new_blocks(apps, schema_editor):
+    Block = apps.get_model("game", "Block")
+
+    block1 = Block.objects.get(type="variables_numeric_set")
+    block2 = Block.objects.get(type="variables_increment")
+    block3 = Block.objects.get(type="variables_get")
+    block4 = Block.objects.get(type="math_number")
+    block5 = Block.objects.get(type="logic_compare")
+
+    block1.delete()
+    block2.delete()
+    block3.delete()
+    block4.delete()
+    block5.delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("game", "0073_level_75_solution"),
+    ]
+
+    operations = [migrations.RunPython(add_new_blocks, reverse_code=remove_new_blocks)]
