45f7448f286e03a08f55210ec03642acd2e32d9d
Dioni Zhong <dioni.zhong@ocado.com>
2021-12-21 16:21:03 +0000

    add improvement table

--- game/views/scoreboard.py
+++ game/views/scoreboard.py
@@ -22,171 +22,17 @@ Headers = ["Class", "Student", "Completed", "Total time"]
 TotalPointsHeader = "Total points"
 
 
-def scoreboard(request):
-    """
-    Renders a page with students' scores. A teacher can see the visible classes in
-    their school. Student's view is restricted to their class if their teacher enabled
-    the scoreboard for said class.
-    """
-    if not permissions.can_see_scoreboard(request.user):
-        return render_no_permission_error(request)
-
-    user = User(request.user.userprofile)
-    users_classes = classes_for(user)
-
-    class_ids = set(map(int, request.POST.getlist("classes")))
-    episode_ids = set(map(int, request.POST.getlist("episodes")))
-    levels_sorted = []
-
-    if user.is_independent_student():
-        return render_no_permission_error(request)
-
-    if is_teacher_with_no_classes_assigned(user, users_classes):
-        return render_no_permission_error(request)
-
-    if not is_valid_request(user, class_ids):
-        raise Http404
-
-    form = ScoreboardForm(request.POST or None, classes=users_classes)
-
-    if request.method == "POST" and form.is_valid():
-        for episode_id in episode_ids:
-            episode = Episode.objects.get(id=episode_id)
-            levels_sorted += episode.levels
-
-        student_data, headers, level_headers = scoreboard_data(
-            user, levels_sorted, class_ids
-        )
-    else:
-        student_data = []
-        headers = []
-        level_headers = []
-
-    csv_export = "export" in request.POST
-
-    if csv_export:
-        return scoreboard_csv(student_data, levels_sorted)
-    else:
-        return scoreboard_view(request, form, student_data, headers, level_headers)
-
-
-def render_no_permission_error(request):
-    return renderError(
-        request, messages.noPermissionTitle(), messages.noPermissionScoreboard()
-    )
-
-
-def is_teacher_with_no_classes_assigned(user, users_classes):
-    return user.is_teacher() and len(users_classes) == 0
-
-
-def success_response(form, student_data, headers):
-    return form, student_data, headers, None
-
-
-def classes_for(user):
-    if user.is_teacher():
-        if user.teacher.is_admin:
-            teachers = Teacher.objects.filter(school=user.teacher.school)
-            return Class.objects.filter(teacher__in=teachers)
-        else:
-            return Class.objects.filter(teacher=user.teacher)
-
-    elif user.is_student():
-        class_ = user.student.class_field
-        return Class.objects.filter(id=class_.id)
-
-
-def scoreboard_view(request, form, student_data, headers, level_headers):
-    database_episodes = level_selection.fetch_episode_data(False)
-
-    return render(
-        request,
-        "game/scoreboard.html",
-        context={
-            "form": form,
-            "student_data": student_data,
-            "headers": headers,
-            "level_headers": level_headers,
-            "total_points_header": TotalPointsHeader,
-            "episodes": database_episodes,
-        },
-    )
-
-
-def scoreboard_data(user, episode_ids, class_ids):
-    classes = Class.objects.filter(id__in=class_ids)
-
-    students = students_visible_to_user(user, classes)
-
-    # Show the total score, total time and score of each level
-    return data_and_headers_for(students, episode_ids)
-
-
-def data_and_headers_for(students, levels_sorted):
-    level_headers = list(map(to_name, levels_sorted))
-    student_data = [student_row(levels_sorted, student) for student in students]
-
-    return student_data, Headers, level_headers
-
-
-def to_name(level):
-    return f"L{level.id}"
-
-
-def sorted_levels_by(level_ids):
-    return sort_levels(Level.objects.filter(id__in=level_ids))
-
-
-def are_classes_viewable_by_teacher(class_ids, user):
-    teachers = Teacher.objects.filter(school=user.teacher.school)
-    classes_in_teachers_school = Class.objects.filter(teacher__in=teachers).values_list(
-        "id", flat=True
-    )
-    for class_id in class_ids:
-        is_authorised = class_id in classes_in_teachers_school
-        if not is_authorised:
-            return False
-    return True
-
-
-def authorised_student_access(class_, class_ids):
-    return len(class_ids) == 1 and next(iter(class_ids)) == class_
-
-
-def students_visible_to_student(student):
-    class_ = student.class_field
-    if is_viewable(class_):
-        return class_.students.filter(new_user__is_active=True).select_related(
-            "class_field", "user__user"
-        )
-    else:
-        return [student]
-
-
-def students_visible_to_user(user, classes):
-    if user.is_teacher():
-        return students_of_classes(classes)
-    elif user.is_student():
-        student = user.student
-        return students_visible_to_student(student)
-
-
-def students_of_classes(classes):
-    return Student.objects.filter(
-        class_field__in=classes, new_user__is_active=True
-    ).select_related("class_field", "user__user")
-
-
-def is_valid_request(user, class_ids):
-    if len(class_ids) == 0:
-        return True
-    elif user.is_teacher():
-        return are_classes_viewable_by_teacher(class_ids, user)
-    elif user.is_student():
-        return authorised_student_access(user.student.class_field.id, class_ids)
-    else:
-        return False
+class StudentRow:
+    def __init__(self, *args, **kwargs):
+        student = kwargs.get("student")
+        self.class_field = student.class_field
+        self.name = student.user.user.first_name
+        self.id = student.id
+        self.total_time = kwargs.get("total_time", timedelta(0))
+        self.total_score = kwargs.get("total_score", 0)
+        self.level_scores = kwargs.get("level_scores", {})
+        self.completed = kwargs.get("completed", 0)
+        self.percentage_complete = kwargs.get("percentage_complete", 0)
 
 
 # Returns row of student object with values and scores of each selected level
@@ -257,25 +103,239 @@ def student_row(levels_sorted, student):
     return row
 
 
+def to_name(level):
+    return f"L{level.id}"
+
+
+def data_and_headers_for(students, levels_sorted):
+    level_headers = list(map(to_name, levels_sorted))
+    student_data = [student_row(levels_sorted, student) for student in students]
+
+    return student_data, Headers, level_headers
+
+
+def scoreboard_data(user, levels_sorted, class_ids):
+    classes = Class.objects.filter(id__in=class_ids)
+
+    students = students_visible_to_user(user, classes)
+
+    # Show the total score, total time and score of each level
+    return data_and_headers_for(students, levels_sorted)
+
+
+class StudentInTrouble:
+    def __init__(self, *args, **kwargs):
+        student = kwargs.get("student")
+        self.class_field = student.class_field
+        self.name = student.user.user.first_name
+        self.areas = kwargs.get("areas")
+
+
+def _check_attempts(student):
+    threshold = 0.5
+
+    # episode ids with low attempts (below 50%)
+    low_episode_ids = set()
+    for episode_id in range(1, 12):
+        episode = Episode.objects.get(id=episode_id)
+        levels = episode.levels
+
+        best_attempts = Attempt.objects.filter(
+            level__in=levels, student=student, is_best_attempt=True
+        ).select_related("level")
+
+        total_score = 0
+        total_possible_score = 0
+        total_max_scores = 0
+        for attempt in best_attempts:
+            max_score = 10 if attempt.level.disable_route_score else 20
+
+            total_score += attempt.score if attempt.score is not None else 0
+            total_possible_score += max_score
+
+            is_low_attempt = attempt.score == 0 or max_score / attempt.score < threshold
+            if is_low_attempt:
+                low_episode_ids.add(episode_id)
+
+    return low_episode_ids
+
+
+# Returns students that need improvement
+def get_improvement_data(class_ids):
+    classes = Class.objects.filter(id__in=class_ids)
+    all_students = students_of_classes(classes)
+
+    the_students = []  # that need improvement
+    for student in all_students:
+        episodes_of_concern = _check_attempts(student)
+        if episodes_of_concern:
+            areas = [messages.get_episode_title(ep_id) for ep_id in episodes_of_concern]
+            areas_summary = ", ".join(areas)
+            the_students.append(StudentInTrouble(student=student, areas=areas_summary))
+    return the_students
+
+
+def scoreboard_view(
+    request, form, student_data, headers, level_headers, improvement_data
+):
+    database_episodes = level_selection.fetch_episode_data(False)
+
+    return render(
+        request,
+        "game/scoreboard.html",
+        context={
+            "form": form,
+            "student_data": student_data,
+            "headers": headers,
+            "level_headers": level_headers,
+            "total_points_header": TotalPointsHeader,
+            "episodes": database_episodes,
+            "improvement_data": improvement_data,
+        },
+    )
+
+
+def scoreboard(request):
+    """
+    Renders a page with students' scores. A teacher can see the visible classes in
+    their school. Student's view is restricted to their class if their teacher enabled
+    the scoreboard for said class.
+    """
+    if not permissions.can_see_scoreboard(request.user):
+        return render_no_permission_error(request)
+
+    user = User(request.user.userprofile)
+    users_classes = classes_for(user)
+
+    class_ids = set(map(int, request.POST.getlist("classes")))
+    episode_ids = set(map(int, request.POST.getlist("episodes")))
+    levels_sorted = []
+
+    if user.is_independent_student():
+        return render_no_permission_error(request)
+
+    if is_teacher_with_no_classes_assigned(user, users_classes):
+        return render_no_permission_error(request)
+
+    if not is_valid_request(user, class_ids):
+        raise Http404
+
+    form = ScoreboardForm(request.POST or None, classes=users_classes)
+
+    if request.method == "POST" and form.is_valid():
+        for episode_id in episode_ids:
+            episode = Episode.objects.get(id=episode_id)
+            levels_sorted += episode.levels
+
+        student_data, headers, level_headers = scoreboard_data(
+            user, levels_sorted, class_ids
+        )
+
+        improvement_data = get_improvement_data(class_ids)
+    else:
+        student_data = []
+        headers = []
+        level_headers = []
+        improvement_data = []
+
+    csv_export = "export" in request.POST
+
+    if csv_export:
+        return scoreboard_csv(student_data, levels_sorted)
+    else:
+        return scoreboard_view(
+            request, form, student_data, headers, level_headers, improvement_data
+        )
+
+
+def render_no_permission_error(request):
+    return renderError(
+        request, messages.noPermissionTitle(), messages.noPermissionScoreboard()
+    )
+
+
+def is_teacher_with_no_classes_assigned(user, users_classes):
+    return user.is_teacher() and len(users_classes) == 0
+
+
+def success_response(form, student_data, headers):
+    return form, student_data, headers, None
+
+
+def classes_for(user):
+    if user.is_teacher():
+        if user.teacher.is_admin:
+            teachers = Teacher.objects.filter(school=user.teacher.school)
+            return Class.objects.filter(teacher__in=teachers)
+        else:
+            return Class.objects.filter(teacher=user.teacher)
+
+    elif user.is_student():
+        class_ = user.student.class_field
+        return Class.objects.filter(id=class_.id)
+
+
+def sorted_levels_by(level_ids):
+    return sort_levels(Level.objects.filter(id__in=level_ids))
+
+
+def are_classes_viewable_by_teacher(class_ids, user):
+    teachers = Teacher.objects.filter(school=user.teacher.school)
+    classes_in_teachers_school = Class.objects.filter(teacher__in=teachers).values_list(
+        "id", flat=True
+    )
+    for class_id in class_ids:
+        is_authorised = class_id in classes_in_teachers_school
+        if not is_authorised:
+            return False
+    return True
+
+
+def authorised_student_access(class_, class_ids):
+    return len(class_ids) == 1 and next(iter(class_ids)) == class_
+
+
+def students_visible_to_student(student):
+    class_ = student.class_field
+    if is_viewable(class_):
+        return class_.students.filter(new_user__is_active=True).select_related(
+            "class_field", "user__user"
+        )
+    else:
+        return [student]
+
+
+def students_visible_to_user(user, classes):
+    if user.is_teacher():
+        return students_of_classes(classes)
+    elif user.is_student():
+        student = user.student
+        return students_visible_to_student(student)
+
+
+def students_of_classes(classes):
+    return Student.objects.filter(
+        class_field__in=classes, new_user__is_active=True
+    ).select_related("class_field", "user__user")
+
+
+def is_valid_request(user, class_ids):
+    if len(class_ids) == 0:
+        return True
+    elif user.is_teacher():
+        return are_classes_viewable_by_teacher(class_ids, user)
+    elif user.is_student():
+        return authorised_student_access(user.student.class_field.id, class_ids)
+    else:
+        return False
+
+
 def is_viewable(class_):
     return class_.classmates_data_viewable
 
 
 def chop_miliseconds(delta):
     return delta - timedelta(microseconds=delta.microseconds)
-
-
-class StudentRow(object):
-    def __init__(self, *args, **kwargs):
-        student = kwargs.get("student")
-        self.class_field = student.class_field
-        self.name = student.user.user.first_name
-        self.id = student.id
-        self.total_time = kwargs.get("total_time", timedelta(0))
-        self.total_score = kwargs.get("total_score", 0)
-        self.level_scores = kwargs.get("level_scores", {})
-        self.completed = kwargs.get("completed", 0)
-        self.percentage_complete = kwargs.get("percentage_complete", 0)
 
 
 class User(object):
