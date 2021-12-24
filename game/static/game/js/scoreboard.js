d7dd1053d8937f699d88bcb859f581781f8cebeb
Razvan Mahu <razvan.mahu@ocado.com>
2021-12-24 12:30:29 +0000

    fix clear not unchecking select-all

--- game/static/game/js/scoreboard.js
+++ game/static/game/js/scoreboard.js
@@ -1,15 +1,21 @@
-$('#clear-classes').on('click', () => {
-    $('[id^="id_classes_"]').prop('checked', false)
-})
+$("#clear-classes").on("click", () => {
+  $('[id^="id_classes_"],#select-all-classes').prop("checked", false);
+});
 
-$('#select-all-classes').on('click', () => {
-    $('[id^="id_classes_"]').prop('checked', $('#select-all-classes').is(':checked'))
-})
+$("#select-all-classes").on("click", () => {
+  $('[id^="id_classes_"]').prop(
+    "checked",
+    $("#select-all-classes").is(":checked")
+  );
+});
 
-$('#clear-levels').on('click', () => {
-    $('[id^="id_episodes_"]').prop('checked', false)
-})
+$("#clear-levels").on("click", () => {
+  $('[id^="id_episodes_"],#select-all-levels').prop("checked", false);
+});
 
-$('#select-all-levels').on('click', () => {
-    $('[id^="id_episodes_"]').prop('checked', $('#select-all-levels').is(':checked'))
-})
+$("#select-all-levels").on("click", () => {
+  $('[id^="id_episodes_"]').prop(
+    "checked",
+    $("#select-all-levels").is(":checked")
+  );
+});
