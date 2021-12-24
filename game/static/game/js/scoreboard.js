de0af574cd12c30bd50400d49f750bed5f931eac
Dioni Zhong <dioni.zhong@ocado.com>
2021-12-24 15:44:55 +0000

    update tests and review fixes

--- game/static/game/js/scoreboard.js
+++ game/static/game/js/scoreboard.js
@@ -13,3 +13,80 @@ $('#clear-levels').on('click', () => {
 $('#select-all-levels').on('click', () => {
     $('[id^="id_episodes_"]').prop('checked', $('#select-all-levels').is(':checked'))
 })
+
+$(document).ready(function() {
+    let scoreboardPresent = document.getElementById("scoreboardTable") !== null;
+    let improvementPresent = document.getElementById("improvementTable") !== null;
+
+    if (scoreboardPresent) {
+        let table = $('#scoreboardTable').DataTable({
+            scrollY: false,
+            scrollX: true,
+            scrollCollapse: false,
+            paging: true,
+            deferRender: true,
+            language: {
+                emptyTable: 'No data available in table',
+                loadingRecords: 'Loading...',
+                processing: 'Processing...',
+                search: 'Search:',
+                zeroRecords: 'No matching records found',
+            },
+            columnDefs: [
+                {
+                    orderable: false,
+                    targets: "no-sort",
+                },
+            ],
+        });
+
+        new $.fn.dataTable.FixedColumns(table, {
+            leftColumns: 3,
+            rightColumns: 1,
+        });
+        document.getElementById("tableWrapper").scrollIntoView();
+
+        $('#scoreboardSearch').on("keyup", function() {
+            table.search(this.value).draw();
+        });
+
+        $("#scoreboardTable_next").append(" >");
+        $(".next").removeClass("paginate_button");
+
+        table.on("draw", () => {
+            $("#scoreboardTable_next").append(" >");
+            $("#scoreboardTable_previous").prepend("< ");
+            $(".next").removeClass("paginate_button");
+            $(".previous").removeClass("paginate_button");
+        });
+
+        $(window).on('load', function() {
+            table.columns.adjust();
+        });
+    }
+
+    // Stops closing the dropdowns when selecting items within it or when pressing the confirmation buttons
+    $(document).on('click', '.dropdown .dropdown-menu', function (e) {
+        e.stopPropagation();
+    });
+
+    if (improvementPresent) {
+        const impTable = $('#improvementTable').DataTable({
+            pageLength: 5
+        });
+
+        $('#scoreboardSearch').on("keyup", function() {
+            impTable.search(this.value).draw();
+        });
+
+        $("#improvementTable_next").append(" >");
+        $(".next").removeClass("paginate_button");
+
+        impTable.on("draw", () => {
+            $("#improvementTable_next").append(" >");
+            $("#improvementTable_previous").prepend("< ");
+            $(".next").removeClass("paginate_button");
+            $(".previous").removeClass("paginate_button");
+        });
+    }
+});
