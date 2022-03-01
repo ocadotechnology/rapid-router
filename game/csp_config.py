b87f93ad82853d7108077384d284e44f67415a31
Miquel Puig <miquelpuigpey@gmail.com>
2022-03-01 17:23:05 +0100

    fix: Allow Crowdin in-context translation resources (#1312)
    * Allow Crowdin in-context translation resources
    
    * fix: Allow Crowdin in-context translation resources

--- game/csp_config.py
+++ game/csp_config.py
@@ -9,11 +9,13 @@ CSP_CONFIG = {
         f"{domain()}/static/game/raphael_image/",
         f"{domain()}/static/game/js/blockly/media/",
         f"{domain()}/static/icons/",
+        "https://cdn.crowdin.com/"
     ),
-    "CSP_STYLE_SRC": ("https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css",),
+    "CSP_STYLE_SRC": ("https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css", "https://cdn.crowdin.com/"),
     "CSP_SCRIPT_SRC": ("'unsafe-eval'", "https://cdn.crowdin.com/"),
     "CSP_FRAME_SRC": (
         f"{domain()}/static/game/image/",
     ),
     "CSP_OBJECT_SRC": (f"{domain()}/static/game/image/",),
+    "CSP_CONNECT_SRC": ("https://crowdin.com/",)
 }
