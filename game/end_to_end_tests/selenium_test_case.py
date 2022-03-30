569996d30a200a2338b84131a4aa4b83448ce473
Razvan Mahu <razvan.mahu@ocado.com>
2022-03-30 12:19:58 +0100

    fix: use a non threaded server in SeleniumTestCase

--- game/end_to_end_tests/selenium_test_case.py
+++ game/end_to_end_tests/selenium_test_case.py
@@ -6,7 +6,26 @@ This solves a bug introduced when upgrading to Django 1.11,
 see more information here: https://github.com/jazzband/django-pipeline/issues/593
 """
 
-from django.contrib.staticfiles.testing import LiveServerTestCase
+from django.core.servers.basehttp import WSGIServer
+from django.test.testcases import (
+    LiveServerTestCase,
+    LiveServerThread,
+    QuietWSGIRequestHandler,
+)
 from django_selenium_clean import SeleniumTestCase
 
+
+class NonThreadedLiveServerThread(LiveServerThread):
+    """
+    Replaces ThreadedWSGIServer with WSGIServer as the threaded one doesn't close the DB connections properly, thus
+    triggering random "DB table locked" errors.
+    """
+
+    def _create_server(self):
+        return WSGIServer(
+            (self.host, self.port), QuietWSGIRequestHandler, allow_reuse_address=False
+        )
+
+
 SeleniumTestCase.__bases__ = (LiveServerTestCase,)
+SeleniumTestCase.server_thread_class = NonThreadedLiveServerThread
