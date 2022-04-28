106b7571effe471b135221c3f42877a2299ddb27
KamilPawel <70412950+KamilPawel@users.noreply.github.com>
2022-04-28 14:56:33 +0100

    fix: remove thank you from emails (#1333)
    * removed Thank you repeating
    
    * remove Code for Life from title

--- game/messages.py
+++ game/messages.py
@@ -2,14 +2,13 @@ from builtins import str
 
 
 def level_creation_email_subject():
-    return "Code For Life : Custom level to moderate"
+    return "Custom level to moderate"
 
 
 def level_creation_email_text_content():
     return (
         "Your student {student_name} from your class {class_name} has created this level: {level_url}. "
         "If you want to moderate it, you can do it on the moderation board: {moderate_url}."
-        "\n\nThanks,\n\nThe Code for Life team.\n{home_url}"
     )
 
 
