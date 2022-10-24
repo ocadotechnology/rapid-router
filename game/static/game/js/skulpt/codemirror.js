ed9612c876ed8561b35a71dad770c87a125363d1
KamilPawel <kamil.sosinski@ocado.com>
2022-10-24 11:12:55 +0100

    fix: adding a fix prefix to commit :)

--- game/static/game/js/skulpt/codemirror.js
+++ game/static/game/js/skulpt/codemirror.js
@@ -3,7 +3,7 @@
 
 // This is CodeMirror (http://codemirror.net), a code editor
 // implemented in JavaScript on top of the browser's DOM.
-//
+
 // You can find some technical background for some of the code below
 // at http://marijnhaverbeke.nl/blog/#cm-internals .
 
