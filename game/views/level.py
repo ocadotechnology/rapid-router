2e340abbf80405850061fea14d08675960cbe6be
KamilPawel <kamil.sosinski@ocado.com>
2023-01-03 14:24:10 +0000

    trying to fix it on the backend

--- game/views/level.py
+++ game/views/level.py
@@ -137,16 +137,10 @@ def play_level(request, level, from_editor=False):
     :template:`game/game.html`
     """
 
-    night_mode = (
-        False if not app_settings.NIGHT_MODE_FEATURE_ENABLED else "night" in request.GET
-    )
+    night_mode = False if not app_settings.NIGHT_MODE_FEATURE_ENABLED else "night" in request.GET
 
-    if not permissions.can_play_level(
-        request.user, level, app_settings.EARLY_ACCESS_FUNCTION(request)
-    ):
-        return renderError(
-            request, messages.noPermissionTitle(), messages.notSharedLevel()
-        )
+    if not permissions.can_play_level(request.user, level, app_settings.EARLY_ACCESS_FUNCTION(request)):
+        return renderError(request, messages.noPermissionTitle(), messages.notSharedLevel())
 
     # Set default level description/hint lookups
     lesson = "description_level_default"
@@ -189,9 +183,7 @@ def play_level(request, level, from_editor=False):
             .first()
         )
         if not attempt:
-            attempt = Attempt(
-                level=level, student=student, score=None, night_mode=night_mode
-            )
+            attempt = Attempt(level=level, student=student, score=None, night_mode=night_mode)
             fetch_workspace_from_last_attempt(attempt)
             attempt.save()
         else:
@@ -218,7 +210,13 @@ def play_level(request, level, from_editor=False):
         model_solution = level.model_solution
 
     return_view = "level_editor" if from_editor else "levels"
+    print(block_data)
+    temp_block_data = []
+    for block in block_data:
+        if block not in temp_block_data:
+            temp_block_data.append(block)
 
+    block_data = temp_block_data
     return render(
         request,
         "game/game.html",
@@ -240,9 +238,7 @@ def play_level(request, level, from_editor=False):
             "character_height": character_height,
             "wreckage_url": wreckage_url,
             "night_mode": night_mode_javascript,
-            "night_mode_feature_enabled": str(
-                app_settings.NIGHT_MODE_FEATURE_ENABLED
-            ).lower(),
+            "night_mode_feature_enabled": str(app_settings.NIGHT_MODE_FEATURE_ENABLED).lower(),
             "model_solution": model_solution,
             "prev_level_url": _prev_level_url(level, request.user, night_mode),
             "next_level_url": _next_level_url(level, request.user, night_mode),
@@ -253,9 +249,7 @@ def play_level(request, level, from_editor=False):
 
 def fetch_workspace_from_last_attempt(attempt):
     latest_attempt = (
-        Attempt.objects.filter(
-            level=attempt.level, student=attempt.student, night_mode=attempt.night_mode
-        )
+        Attempt.objects.filter(level=attempt.level, student=attempt.student, night_mode=attempt.night_mode)
         .order_by("-start_time")
         .first()
     )
@@ -271,23 +265,15 @@ def delete_level(request, levelID):
         level_management.delete_level(level)
         success = True
 
-    return HttpResponse(
-        json.dumps({"success": success}), content_type="application/javascript"
-    )
+    return HttpResponse(json.dumps({"success": success}), content_type="application/javascript")
 
 
 def submit_attempt(request):
     """Processes a request on submission of the program solving the current level."""
-    if (
-        not request.user.is_anonymous
-        and request.method == "POST"
-        and hasattr(request.user.userprofile, "student")
-    ):
+    if not request.user.is_anonymous and request.method == "POST" and hasattr(request.user.userprofile, "student"):
         level = get_object_or_404(Level, id=request.POST.get("level", 1))
         student = request.user.userprofile.student
-        attempt = Attempt.objects.filter(
-            level=level, student=student, finish_time__isnull=True
-        ).first()
+        attempt = Attempt.objects.filter(level=level, student=student, finish_time__isnull=True).first()
         if attempt:
             attempt.score = float(request.POST.get("score"))
             attempt.workspace = request.POST.get("workspace")
