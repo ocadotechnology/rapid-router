a135f01f1c0f831b82096c0c46c3ef3a0715b6a2
Seb Palmer <s.palmer1@ocado.com>
2023-10-06 14:32:50 +0100

    wip: fixed error, no test

--- game/static/game/js/level_editor.js
+++ game/static/game/js/level_editor.js
@@ -2228,6 +2228,7 @@ ocargo.LevelEditor = function(levelId) {
     }
 
     function restoreState(state) {
+        console.log("restore state");
 
         clear();
 
@@ -2331,8 +2332,10 @@ ocargo.LevelEditor = function(levelId) {
         }
 
         // Load in language data
-        var languageSelect = $('#languageSelect');
-        if(state.blocklyEnabled && state.pythonEnabled) {
+        var languageSelect = $('#language_select');
+        if (state.blocklyEnabled && state.pythonViewEnabled){
+            languageSelect.val('blocklyWithPythonView');
+        } else if(state.blocklyEnabled && state.pythonEnabled) {
             languageSelect.val('both');
         } else if(state.pythonEnabled) {
             languageSelect.val('python');
