0e4fd0cc39c3bf98cdc7a1022c750d51a96859a1
faucomte97 <f.aucomte@hotmail.co.uk>
2023-10-16 17:27:30 +0100

    Update migrations

--- game/migrations/0084_alter_block_block_type.py
+++ game/migrations/0084_alter_block_block_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('game', '0082_level_43_solution'),
+        ('game', '0083_add_cows_to_existing_levels'),
     ]
 
     operations = [
