808ed688397d1d899a1f772bb92ce5192854bd8a
Eve Martin <eve.martin@ocado.com>
2024-05-13 14:47:25 +0000

    debug test

--- game/end_to_end_tests/test_level_editor.py
+++ game/end_to_end_tests/test_level_editor.py
@@ -26,20 +26,19 @@ class TestLevelEditor(BaseGameTest):
         self.go_to_level_editor()
         add_road_button = self.selenium.find_element(By.ID, "add_road")
         add_road_button.click()
-
         action.move_to_element(add_road_button).move_by_offset(600, 0).click_and_hold().move_by_offset(300, 0).click()
+
         add_house_button = self.selenium.find_element(By.ID, "add_house")
         add_house_button.click()
-
         action.move_to_element(add_road_button).move_by_offset(600, 0).click()
         action.move_to_element(add_road_button).move_by_offset(800, 0).click()
 
-        added_houses = self.selenium.find_elements(By.CSS_SELECTOR, "rect[fill='#0000ff']")
+        added_houses = self.selenium.find_elements(By.XPATH, "//rect[fill='#0000ff']")
         assert len(added_houses) == 2
 
         delete_house_button = self.selenium.find_element(By.ID, "delete_house")
         delete_house_button.click()
-
         action.move_to_element(delete_house_button).move_by_offset(600, 0).click()
-        houses_after_delete = self.selenium.find_elements(By.CSS_SELECTOR, "rect[fill='#0000ff']")
+
+        houses_after_delete = self.selenium.find_elements(By.XPATH, "//rect[fill='#0000ff']")
         assert len(houses_after_delete) == 1
