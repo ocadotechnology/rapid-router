8a376cc76ca5638d70e7c26e7c95c3adf2454342
Eve Martin <eve.martin@ocado.com>
2024-05-13 12:36:26 +0000

    write test

--- game/end_to_end_tests/test_level_editor.py
+++ game/end_to_end_tests/test_level_editor.py
@@ -1,5 +1,6 @@
 from game.end_to_end_tests.base_game_test import BaseGameTest
 from game.views.level_editor import available_blocks
+from selenium.webdriver.common.by import By
 
 
 class TestLevelEditor(BaseGameTest):
@@ -17,3 +18,25 @@ class TestLevelEditor(BaseGameTest):
         for block_type in block_types:
             assert page.element_exists_by_id(f"{block_type}_checkbox")
             assert page.element_exists_by_id(f"{block_type}_image")
+
+    def test_multiple_houses(self):
+        self.go_to_level_editor()
+        add_road_button = self.selenium.find_element(By.ID, "add_road")
+        add_road_button.click()
+
+        self.selenium.move_to_element(add_road_button).move_by_offset(600, 0).click_and_hold().move_by_offset(300, 0).click()
+        add_house_button = self.selenium.find_element(By.ID, "add_house")
+        add_house_button.click()
+
+        self.selenium.move_to_element(add_road_button).move_by_offset(600, 0).click()
+        self.selenium.move_to_element(add_road_button).move_by_offset(800, 0).click()
+
+        added_houses = self.selenium.findElements(By.CSS_SELECTOR, "rect[fill='#0000ff']")
+        assert len(added_houses) == 2
+
+        delete_house_button = self.selenium.find_element(By.ID, "delete_house")
+        delete_house_button.click()
+
+        self.selenium.move_to_element(delete_house_button).move_by_offset(600, 0).click()
+        houses_after_delete = self.selenium.findElements(By.CSS_SELECTOR, "rect[fill='#0000ff']")
+        assert len(houses_after_delete) == 1
