036e267016a5f9dbc85aa6d558204196de19bb32
Eve Martin <eve.martin@ocado.com>
2024-05-14 09:51:00 +0000

    fix typo

--- game/end_to_end_tests/test_level_editor.py
+++ game/end_to_end_tests/test_level_editor.py
@@ -24,8 +24,10 @@ class TestLevelEditor(BaseGameTest):
         add_road_button = self.selenium.find_element(By.ID, "add_road")
         add_road_button.click()
 
-        road_start = self.selenium.find_element(By.CSS_SELECTOR, "image[x='130'][y='530']")
-        road_end = self.selenium.find_element(By.CSS_SELECTOR, "image[x='330'][y='530']")
+
+
+        road_start = self.selenium.find_element(By.CSS_SELECTOR, "rect[x='130'][y='530']")
+        road_end = self.selenium.find_element(By.CSS_SELECTOR, "rect[x='330'][y='530']")
         ActionChains(self.selenium).drag_and_drop(road_start, road_end).perform()
 
         road_image = self.selenium.find_elements(By.CSS_SELECTOR, "image[href='/static/game/raphael_image/road_tiles/road/dead_end.svg']")
