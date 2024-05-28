372c5b177809bde5219a74e6820cbb3b115d7553
Eve Martin <eve.martin@ocado.com>
2024-05-28 12:31:51 +0000

    add back test code

--- game/end_to_end_tests/test_level_editor.py
+++ game/end_to_end_tests/test_level_editor.py
@@ -6,7 +6,7 @@ from game.views.level_editor import available_blocks
 
 
 class TestLevelEditor(BaseGameTest):
-    def test_set_up_basic_map(self):
+    def set_up_basic_map(self):
         self.test_level_editor_displays()
 
         add_road_button = self.selenium.find_element(By.ID, "add_road")
@@ -19,14 +19,10 @@ class TestLevelEditor(BaseGameTest):
         mark_start_button = self.selenium.find_element(By.ID, "start")
         mark_start_button.click()
         ActionChains(self.selenium).move_to_element(road_start).click().perform()
-        start_space = self.selenium.find_elements(By.CSS_SELECTOR, "rect[fill='#ff0000']")
-        assert len(start_space) == 1
 
         add_house_button = self.selenium.find_element(By.ID, "add_house")
         add_house_button.click()
         ActionChains(self.selenium).move_to_element(road_end).click().perform()
-        house_space = self.selenium.find_elements(By.CSS_SELECTOR, "rect[fill='#0000ff']")
-        assert len(house_space) == 1
 
         return [road_start, road_end]
 
@@ -46,7 +42,7 @@ class TestLevelEditor(BaseGameTest):
             assert page.element_exists_by_id(f"{block_type}_image")
 
     def test_multiple_houses(self):
-        [road_start, road_end] = self.test_set_up_basic_map()
+        [road_start, road_end] = self.set_up_basic_map()
 
         road_middle = self.selenium.find_element(By.CSS_SELECTOR, "rect[x='230'][y='530']")
 
@@ -66,7 +62,10 @@ class TestLevelEditor(BaseGameTest):
         assert len(houses_after_delete) == 1
 
     def test_cow_on_origin(self):
-        [road_start, road_end] = self.test_set_up_basic_map()
+        [road_start, road_end] = self.set_up_basic_map()
+
+        house_space = self.selenium.find_elements(By.CSS_SELECTOR, "rect[fill='#ff0000']")
+        assert len(house_space) == 1
 
         scenery_tab = self.selenium.find_element(By.ID, "scenery_tab")
         scenery_tab.click()
@@ -85,36 +84,14 @@ class TestLevelEditor(BaseGameTest):
         house_space = self.selenium.find_elements(By.CSS_SELECTOR, "rect[fill='#0000ff']")
         assert len(house_space) == 1
 
-    # def test_cow_on_house(self):
-    #     add_road_button = self.selenium.find_element(By.ID, "add_road")
-    #     self.wait_for_element_to_be_clickable(add_road_button, wait_seconds=15)
-    #     add_road_button.click()
+        scenery_tab = self.selenium.find_element(By.ID, "scenery_tab")
+        scenery_tab.click()
 
-    #     road_start = self.selenium.find_element(By.CSS_SELECTOR, "rect[x='130'][y='530']")
-    #     road_end = self.selenium.find_element(By.CSS_SELECTOR, "rect[x='330'][y='530']")
-    #     ActionChains(self.selenium).drag_and_drop(road_start, road_end).perform()
+        cow = self.selenium.find_element(By.CSS_SELECTOR, "img[id='cow']")
+        cow.click()
 
-    #     mark_start_button = self.selenium.find_element(By.ID, "start")
-    #     mark_start_button.click()
-    #     ActionChains(self.selenium).move_to_element(road_start).click().perform()
-
-    #     add_house_button = self.selenium.find_element(By.ID, "add_house")
-    #     add_house_button.click()
-    #     ActionChains(self.selenium).move_to_element(road_end).click().perform()
-
-    #     scenery_tab = self.selenium.find_element(By.ID, "scenery_tab")
-    #     scenery_tab.click()
-
-    #     cow = self.selenium.find_element(By.CSS_SELECTOR, "img[id='cow']")
-    #     cow.click()
-
-    #     draggable_cow = self.selenium.find_element(By.CSS_SELECTOR, "image[x='0'][y='0']")
-    #     ActionChains(self.selenium).click_and_hold(draggable_cow).move_to_element(road_end).perform()
-    #     boxes = self.selenium.find_elements(By.CSS_SELECTOR, "rect")
-    #     for box in boxes:
-    #         print(box.get_attribute("x"))
-    #         print(box.get_attribute("y"))
-    #         print(box.get_attribute("fill"))
-    #     forbidden_end_space = self.selenium.find_elements(By.CSS_SELECTOR, "rect[fill='#e35f4d'][fill-opacity='0.7'][x='330'][y='530']")
-    #     assert len(forbidden_end_space) == 1
+        draggable_cow = self.selenium.find_element(By.CSS_SELECTOR, "image[x='0'][y='0']")
+        ActionChains(self.selenium).click_and_hold(draggable_cow).move_to_element(road_end).perform()
+        forbidden_end_space = self.selenium.find_elements(By.CSS_SELECTOR, "rect[fill='#e35f4d'][fill-opacity='0.7'][x='330'][y='530']")
+        assert len(forbidden_end_space) == 1
         
