bbb7242f877c60962902a48d5aa9d2b1bed2d76e
Eve Martin <eve.martin@ocado.com>
2024-08-14 16:34:39 +0000

    add level urls

--- game/python_den_urls.py
+++ game/python_den_urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 
+from game.views.level import play_default_level
 from game.views.level_selection import python_levels
 
 urlpatterns = [
     url(r"^$", python_levels, name="python_levels"),
+    url(r"^(?P<levelName>[A-Z0-9]+)/$", play_default_level, name="play_default_level"),
 ]
