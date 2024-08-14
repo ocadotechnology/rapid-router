c8c5fdc84bb9d053f8474c0e7db9a4ae121d9ec7
Eve Martin <eve.martin@ocado.com>
2024-08-14 19:14:19 +0000

    make small updates

--- game/python_den_urls.py
+++ game/python_den_urls.py
@@ -5,5 +5,5 @@ from game.views.level_selection import python_levels
 
 urlpatterns = [
     url(r"^$", python_levels, name="python_levels"),
-    url(r"^(?P<levelName>[A-Z0-9]+)/$", play_default_level, name="play_default_level"),
+    url(r"^(?P<levelName>[A-Z0-9]+)/$", play_default_level, name="play_python_default_level"),
 ]
