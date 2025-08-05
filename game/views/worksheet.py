b3f25afe436d953e84312b766a40ed836019b3d8
faucomte97 <f.aucomte@hotmail.co.uk>
2025-08-05 14:49:18 +0100

    Remove irrelevant docstring

--- game/views/worksheet.py
+++ game/views/worksheet.py
@@ -6,25 +6,6 @@ from game.models import Worksheet
 
 
 def worksheet(request, worksheetId):
-    """Loads a level for rendering in the game.
-
-    **Context**
-
-    ``RequestContext``
-    ``level``
-        Level that is about to be played. An instance of :model:`game.Level`.
-    ``blocks``
-        Blocks that are available during the game. List of :model:`game.Block`.
-    ``lesson``
-        Instruction shown at the load of the level. String from `game.messages`.
-    ``hint``
-        Hint shown after a number of failed attempts. String from `game.messages`.
-
-    **Template:**
-
-    :template:`game/game.html`
-    """
-
     worksheet = Worksheet.objects.get(pk=worksheetId)
 
     return render(
