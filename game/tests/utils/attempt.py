7c74ca5d999e56e795fb4c40b145fe9e62c69c2c
faucomte97 <f.aucomte@hotmail.co.uk>
2025-11-19 15:53:45 +0000

    Update test util

--- game/tests/utils/attempt.py
+++
@@ -1,8 +0,0 @@
-from game.models import Attempt
-
-
-def create_attempt(student, level, score):
-    attempt = Attempt(level=level, student=student, score=score)
-    attempt.save()
-
-    return attempt
