79924f909ff8320881db61a88091e90932ff93e8
SKairinos <skairinos@gmail.com>
2026-02-05 10:32:37 +0000

    get user model

--- game/tests/utils/user.py
+++ game/tests/utils/user.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 def get_superuser():
     """Get a superuser for testing, or create one if there isn't one."""
