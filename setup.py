76f2ea503549048be7657db471b22f3c00e3b134
SKairinos <skairinos@gmail.com>
2026-02-19 09:34:35 +0000

    fix

--- setup.py
+++ setup.py
@@ -27,12 +27,18 @@ def parse_requirements(packages: t.Dict[str, t.Dict[str, t.Any]]):
     requirements: t.List[str] = []
     for name, package in packages.items():
         requirement = name
-        if "extras" in package:
-            requirement += f"[{','.join(package['extras'])}]"
-        if "version" in package:
+        if "git" in package:
+            requirement += f" @ git+{package['git']}"
+            if "ref" in package:
+                requirement += f"@{package['ref']}"
+            if "subdirectory" in package:
+                requirement += f"#subdirectory={package['subdirectory']}"
+        elif "version" in package:
+            if "extras" in package:
+                requirement += f"[{','.join(package['extras'])}]"
             requirement += package["version"]
-        if "markers" in package:
-            requirement += f"; {package['markers']}"
+            if "markers" in package:
+                requirement += f"; {package['markers']}"
         requirements.append(requirement)
 
     return requirements
