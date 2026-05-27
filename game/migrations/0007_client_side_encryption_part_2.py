8f0769548fc88e5185b3dca68e2fb4db7d528179
SKairinos <skairinos@gmail.com>
2026-05-27 09:42:04 +0000

    remove plain fields

--- game/migrations/0007_client_side_encryption_part_2.py
+++ game/migrations/0007_client_side_encryption_part_2.py
@@ -6,98 +6,99 @@ from django.db import migrations
 
 level_migrations = [
     # Name
-    migrations.RenameField(
+    migrations.RemoveField(
         model_name="level",
-        old_name="name",
-        new_name="_name_plain",
+        name="_name_plain",
     ),
-    migrations.AddField(
+    migrations.AlterField(
         model_name="level",
         name="_name_enc",
         field=EncryptedTextField(
             associated_data="name",
-            null=True,
             verbose_name="name",
             db_column="name_enc",
+            default=b"",
         ),
+        preserve_default=False,
     ),
-    migrations.AddField(
+    migrations.AlterField(
         model_name="level",
         name="_name_hash",
         field=Sha256Field(
-            null=True,
             editable=False,
             max_length=64,
             verbose_name="name hash",
             db_column="name_hash",
+            default="",
         ),
+        preserve_default=False,
     ),
     # Subtitle
-    migrations.RenameField(
+    migrations.RemoveField(
         model_name="level",
-        old_name="subtitle",
-        new_name="_subtitle_plain",
+        name="_subtitle_plain",
     ),
-    migrations.AddField(
+    migrations.AlterField(
         model_name="level",
         name="_subtitle_enc",
         field=EncryptedTextField(
             associated_data="subtitle",
-            null=True,
             verbose_name="subtitle",
             db_column="subtitle_enc",
+            default=b"",
         ),
+        preserve_default=False,
     ),
     # Lesson
-    migrations.RenameField(
+    migrations.RemoveField(
         model_name="level",
-        old_name="lesson",
-        new_name="_lesson_plain",
+        name="_lesson_plain",
     ),
-    migrations.AddField(
+    migrations.AlterField(
         model_name="level",
         name="_lesson_enc",
         field=EncryptedTextField(
             associated_data="lesson",
-            null=True,
             verbose_name="lesson",
             db_column="lesson_enc",
+            default=b"",
         ),
+        preserve_default=False,
     ),
     # Hint
-    migrations.RenameField(
+    migrations.RemoveField(
         model_name="level",
-        old_name="hint",
-        new_name="_hint_plain",
+        name="_hint_plain",
     ),
-    migrations.AddField(
+    migrations.AlterField(
         model_name="level",
         name="_hint_enc",
         field=EncryptedTextField(
             associated_data="hint",
-            null=True,
             verbose_name="hint",
             db_column="hint_enc",
+            default=b"",
         ),
+        preserve_default=False,
     ),
 ]
 
 workspace_migrations = [
     # Name
-    migrations.RenameField(
+    migrations.RemoveField(
         model_name="workspace",
-        old_name="name",
-        new_name="_name_plain",
+        name="_name_plain",
     ),
-    migrations.AddField(
+    migrations.AlterField(
         model_name="workspace",
         name="_name_enc",
         field=EncryptedTextField(
             associated_data="name",
-            null=True,
             verbose_name="name",
             db_column="name_enc",
+            default=b"",
         ),
+        preserve_default=False,
     ),
 ]
 
@@ -105,8 +106,8 @@ workspace_migrations = [
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("game", "0005_alter_dailyactivity_unique_together_and_more"),
-        ("user", "0003_client_side_encryption_part_1"),
+        ("game", "0006_client_side_encryption_part_1"),
+        ("user", "0006_client_side_encryption_part_4"),
     ]
 
     operations = [
--- game/models.py
+++ game/models.py
@@ -1,11 +1,13 @@
 import typing as t
 from builtins import str
 
+from codeforlife.legacy.models import Class, Student, UserProfile
 from codeforlife.models import EncryptedModel
 from codeforlife.models.fields import EncryptedTextField, Sha256Field
-from codeforlife.legacy.models import Class, Student, UserProfile
+from codeforlife.models.fields.decorators import validated_field_setter
+from codeforlife.types import Validators
 from django.contrib.auth import get_user_model
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxLengthValidator, MaxValueValidator
 from django.db import models
 from django.db.models import Q
 from django.db.models.query import QuerySet
@@ -142,10 +144,10 @@ def sort_levels(levels):
 class Level(EncryptedModel):
     associated_data = "level"
     field_aliases = {
-        "name": {"_name_plain", "_name_enc", "_name_hash"},
-        "subtitle": {"_subtitle_plain", "_subtitle_enc"},
-        "lesson": {"_lesson_plain", "_lesson_enc"},
-        "hint": {"_hint_plain", "_hint_enc"},
+        "name": {"_name_enc", "_name_hash"},
+        "subtitle": {"_subtitle_enc"},
+        "lesson": {"_lesson_enc"},
+        "hint": {"_hint_enc"},
     }
 
     after_worksheet: t.Optional["Worksheet"]
@@ -155,32 +157,28 @@ class Level(EncryptedModel):
     # --------------------------------------------------------------------------
 
     _name_hash = Sha256Field(
-        verbose_name=_("name hash"), db_column="name_hash", null=True
+        verbose_name=_("name hash"),
+        db_column="name_hash",
     )
-    _name_plain = models.CharField(max_length=100)
     _name_enc = EncryptedTextField(
         associated_data="name",
         db_column="name_enc",
-        null=True,
         verbose_name=_("name"),
     )
+
+    name_validators: Validators = [MaxLengthValidator(100)]
 
     @property
     def name(self):
         """The level's name."""
-        if not self.default and self._name_enc is not None:
-            return EncryptedTextField.get(self, "_name_enc")
-        return self._name_plain
+        return EncryptedTextField.get(self, "_name_enc")
 
     @name.setter
+    @validated_field_setter(*name_validators)
     def name(self, value: str):
         """Set the level's name."""
-        if self.default:
-            self._name_plain = value
-        else:
-            self._name_plain = value  # TODO: remove when all are encrypted.
-            EncryptedTextField.set(self, value, "_name_enc")
-            Sha256Field.set(self, value, "_name_hash")
+        EncryptedTextField.set(self, value, "_name_enc")
+        Sha256Field.set(self, value, "_name_hash")
 
     # --------------------------------------------------------------------------
 
@@ -239,90 +237,72 @@ class Level(EncryptedModel):
     # Subtitle
     # --------------------------------------------------------------------------
 
-    _subtitle_plain = models.TextField(max_length=100, blank=True, null=True)
     _subtitle_enc = EncryptedTextField(
         associated_data="subtitle",
         db_column="subtitle_enc",
-        null=True,
         verbose_name=_("subtitle"),
     )
+
+    subtitle_validators: Validators = [MaxLengthValidator(100)]
 
     @property
     def subtitle(self):
         """The level's subtitle."""
-        if not self.default and self._subtitle_enc is not None:
-            return EncryptedTextField.get(self, "_subtitle_enc")
-        return self._subtitle_plain
+        return EncryptedTextField.get(self, "_subtitle_enc")
 
     @subtitle.setter
-    def subtitle(self, value: t.Optional[str]):
+    @validated_field_setter(*subtitle_validators, blank=True)
+    def subtitle(self, value: str):
         """Set the level's subtitle."""
-        if self.default:
-            self._subtitle_plain = value
-        else:
-            self._subtitle_plain = value  # TODO: remove when all are encrypted.
-            EncryptedTextField.set(self, value, "_subtitle_enc")
+        EncryptedTextField.set(self, value, "_subtitle_enc")
 
     # --------------------------------------------------------------------------
     # Lesson
     # --------------------------------------------------------------------------
 
-    _lesson_plain = models.TextField(
-        max_length=10000, default="Can you find the shortest route?"
-    )
     _lesson_enc = EncryptedTextField(
         associated_data="lesson",
         db_column="lesson_enc",
-        null=True,
         verbose_name=_("lesson"),
     )
+
+    lesson_default = "Can you find the shortest route?"
+    lesson_validators: Validators = [MaxLengthValidator(10000)]
 
     @property
     def lesson(self):
         """The level's description."""
-        if not self.default and self._lesson_enc is not None:
-            return EncryptedTextField.get(self, "_lesson_enc")
-        return self._lesson_plain
+        return EncryptedTextField.get(self, "_lesson_enc")
 
     @lesson.setter
+    @validated_field_setter(*lesson_validators)
     def lesson(self, value: str):
         """Set the level's description."""
-        if self.default:
-            self._lesson_plain = value
-        else:
-            self._lesson_plain = value  # TODO: remove when all are encrypted.
-            EncryptedTextField.set(self, value, "_lesson_enc")
+        EncryptedTextField.set(self, value, "_lesson_enc")
 
     # --------------------------------------------------------------------------
     # Hint
     # --------------------------------------------------------------------------
 
-    _hint_plain = models.TextField(
-        max_length=10000,
-        default="Think back to earlier levels. What did you learn?",
-    )
     _hint_enc = EncryptedTextField(
         associated_data="hint",
         db_column="hint_enc",
-        null=True,
         verbose_name=_("hint"),
     )
+
+    hint_default = "Think back to earlier levels. What did you learn?"
+    hint_validators: Validators = [MaxLengthValidator(10000)]
 
     @property
     def hint(self):
         """The level's hint."""
-        if not self.default and self._hint_enc is not None:
-            return EncryptedTextField.get(self, "_hint_enc")
-        return self._hint_plain
+        return EncryptedTextField.get(self, "_hint_enc")
 
     @hint.setter
+    @validated_field_setter(*hint_validators)
     def hint(self, value: str):
         """Set the level's hint."""
-        if self.default:
-            self._hint_plain = value
-        else:
-            self._hint_plain = value  # TODO: remove when all are encrypted.
-            EncryptedTextField.set(self, value, "_hint_enc")
+        EncryptedTextField.set(self, value, "_hint_enc")
 
     # --------------------------------------------------------------------------
 
@@ -368,28 +348,6 @@ class Level(EncryptedModel):
 
     class Meta:
         constraints = [
-            # TODO: add these checks once all data is encrypted.
-            # models.CheckConstraint(
-            #     condition=~Q(
-            #         default=True,
-            #         _name_enc__isnull=False,
-            #     ),
-            #     name="level__default_name_is_not_encrypted",
-            # ),
-            # models.CheckConstraint(
-            #     condition=~Q(
-            #         default=True,
-            #         _name_hash__isnull=False,
-            #     ),
-            #     name="level__default_name_is_not_hashed",
-            # ),
-            # models.CheckConstraint(
-            #     condition=~Q(
-            #         default=False,
-            #         _name_plain__isnull=False,
-            #     ),
-            #     name="level__non_default_name_is_not_plain",
-            # ),
             models.CheckConstraint(
                 condition=~Q(
                     default=True,
@@ -460,32 +418,30 @@ class LevelDecor(models.Model):
 class Workspace(EncryptedModel):
     associated_data = "workspace"
     field_aliases = {
-        "name": {"_name_plain", "_name_enc"},
+        "name": {"_name_enc"},
     }
 
     # --------------------------------------------------------------------------
     # Name
     # --------------------------------------------------------------------------
 
-    _name_plain = models.CharField(max_length=200)
     _name_enc = EncryptedTextField(
         associated_data="name",
         db_column="name_enc",
-        null=True,
         verbose_name=_("name"),
     )
+
+    name_validators: Validators = [MaxLengthValidator(200)]
 
     @property
     def name(self):
         """The level's name."""
-        if self._name_enc is not None:
-            return EncryptedTextField.get(self, "_name_enc")
-        return self._name_plain
+        return EncryptedTextField.get(self, "_name_enc")
 
     @name.setter
+    @validated_field_setter(*name_validators)
     def name(self, value: str):
         """Set the level's name."""
-        self._name_plain = value
         EncryptedTextField.set(self, value, "_name_enc")
 
     # --------------------------------------------------------------------------
